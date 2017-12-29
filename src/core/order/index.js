79fb1a92057503d57ea1d3285c0263ed2af99544
David Chin <davidchin.yf@gmail.com>
2017-12-29 13:21:57 +1100

    refactor(order): CHECKOUT-2419 Remove unused OrderSummarySelectorMixin

--- src/core/order/index.js
+++ src/core/order/index.js
@@ -1,6 +1,5 @@
-export { default as PlaceOrderService } from './place-order-service';
 export { default as OrderActionCreator } from './order-action-creator';
 export { default as orderReducer } from './order-reducer';
 export { default as OrderRequestSender } from './order-request-sender';
 export { default as OrderSelector } from './order-selector';
-export { default as OrderSummarySelectorMixin } from './order-summary-selector-mixin';
+export { default as PlaceOrderService } from './place-order-service';
--- src/core/order/order.typedef.js
+++ src/core/order/order.typedef.js
@@ -1,6 +1,42 @@
 /**
  * @typedef {Object} Order
- * @extends OrderSummary
+ * @property {string} id
+ * @property {Object[]} items
+ * @property {string} currency
+ * @property {Object} subtotal
+ * @property {number} subtotal.amount
+ * @property {number} subtotal.integerAmount
+ * @property {Object} coupon
+ * @property {number} coupon.discountedAmount
+ * @property {string[]} subtotal.coupons
+ * @property {Object} discount
+ * @property {number} discount.amount
+ * @property {number} discount.integerAmount
+ * @property {string[]} discountNotifications
+ * @property {Object} giftCertificate
+ * @property {number} giftCertificate.totalDiscountedAmount
+ * @property {string[]} giftCertificate.appliedGiftCertificates
+ * @property {Object} shipping
+ * @property {number} shipping.amount
+ * @property {number} shipping.integerAmount
+ * @property {number} shipping.amountBeforeDiscount
+ * @property {number} shipping.integerAmountBeforeDiscount
+ * @property {boolean} shipping.required
+ * @property {Object} storeCredit
+ * @property {number} storeCredit.amount
+ * @property {Object} taxSubtotal
+ * @property {number} taxSubtotal.amount
+ * @property {number} taxSubtotal.integerAmount
+ * @property {Object[]} taxes
+ * @property {Object} taxTotal
+ * @property {number} taxTotal.amount
+ * @property {number} taxTotal.integerAmount
+ * @property {Object} handling
+ * @property {number} handling.amount
+ * @property {number} handling.integerAmount
+ * @property {Object} grandTotal
+ * @property {number} grandTotal.amount
+ * @property {number} grandTotal.integerAmount
  * @property {string} orderId
  * @property {string} token
  * @property {Object} payment
