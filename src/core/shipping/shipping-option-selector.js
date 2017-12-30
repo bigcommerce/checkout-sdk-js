be02b059f17c11b1666d493ed22007d9f62013f8
David Chin <davidchin.yf@gmail.com>
2017-12-30 21:21:24 +1100

    refactor(shipping): CHECKOUT-2784 Simplify ShippingOptionSelector constructor

--- src/core/shipping/shipping-option-selector.js
+++ src/core/shipping/shipping-option-selector.js
@@ -7,24 +7,22 @@ export default class ShippingOptionSelector {
      * @param {QuoteState} quote
      */
     constructor(shippingOptions = {}, quote = {}) {
-        this._shippingOptions = shippingOptions.data;
-        this._quote = quote.data;
-        this._errors = shippingOptions.errors;
-        this._statuses = shippingOptions.statuses;
+        this._shippingOptions = shippingOptions;
+        this._quote = quote;
     }
 
     /**
      * @return {ShippingOptionList}
      */
     getShippingOptions() {
-        return this._shippingOptions;
+        return this._shippingOptions.data;
     }
 
     /**
      * @return {?ShippingOption}
      */
     getSelectedShippingOption() {
-        const { shippingAddress, shippingOption: optionId } = this._quote;
+        const { shippingAddress, shippingOption: optionId } = this._quote.data;
         const shippingOptions = shippingAddress ? this.getShippingOptions()[shippingAddress.id] : undefined;
 
         return find(shippingOptions, { id: optionId });
@@ -34,27 +32,27 @@ export default class ShippingOptionSelector {
      * @return {?ErrorResponse}
      */
     getLoadError() {
-        return this._errors && this._errors.loadError;
+        return this._shippingOptions.errors && this._shippingOptions.errors.loadError;
     }
 
     /**
      * @return {?ErrorResponse}
      */
     getSelectError() {
-        return this._errors && this._errors.selectError;
+        return this._shippingOptions.errors && this._shippingOptions.errors.selectError;
     }
 
     /**
      * @return {boolean}
      */
     isLoading() {
-        return !!(this._statuses && this._statuses.isLoading);
+        return !!(this._shippingOptions.statuses && this._shippingOptions.statuses.isLoading);
     }
 
     /**
      * @return {boolean}
      */
     isSelecting() {
-        return !!(this._statuses && this._statuses.isSelecting);
+        return !!(this._shippingOptions.statuses && this._shippingOptions.statuses.isSelecting);
     }
 }
