e37f8cd6d9e5b3fd8717b2b0603581ed90aced20
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2018-02-05 17:15:55 +1100

    feat(common): CHECKOUT-2416 Complete Config Reducer

--- src/core/config/config-reducer.js
+++ src/core/config/config-reducer.js
@@ -1,12 +1,16 @@
 import { combineReducers } from '../../data-store';
+import * as configActionType from './config-action-types';
 
 /**
  * @param {ConfigState} state
+ * @param {Action} action
  * @return {ConfigState}
  */
 export default function configReducer(state = {}, action) {
     const reducer = combineReducers({
         data: dataReducer,
+        errors: errorsReducer,
+        statuses: statusesReducer,
     });
 
     return reducer(state, action);
@@ -15,8 +19,54 @@ export default function configReducer(state = {}, action) {
 /**
  * @private
  * @param {?Config} data
+ * @param {Action} action
  * @return {?Config}
  */
-function dataReducer(data) {
-    return data;
+function dataReducer(data, action) {
+    switch (action.type) {
+    case configActionType.LOAD_CONFIG_SUCCEEDED:
+        return action.payload ? { ...data, ...action.payload } : data;
+
+    default:
+        return data;
+    }
+}
+
+/**
+ * @private
+ * @param {Object} errors
+ * @param {Action} action
+ * @return {Object}
+ */
+function errorsReducer(errors = {}, action) {
+    switch (action.type) {
+    case configActionType.LOAD_CONFIG_SUCCEEDED:
+        return { ...errors, loadError: undefined };
+
+    case configActionType.LOAD_CONFIG_FAILED:
+        return { ...errors, loadError: action.payload };
+
+    default:
+        return errors;
+    }
+}
+
+/**
+ * @private
+ * @param {Object} statuses
+ * @param {Action} action
+ * @return {Object}
+ */
+function statusesReducer(statuses = {}, action) {
+    switch (action.type) {
+    case configActionType.LOAD_CONFIG_REQUESTED:
+        return { ...statuses, isLoading: true };
+
+    case configActionType.LOAD_CONFIG_SUCCEEDED:
+    case configActionType.LOAD_CONFIG_FAILED:
+        return { ...statuses, isLoading: false };
+
+    default:
+        return statuses;
+    }
 }
