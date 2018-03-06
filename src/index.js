42241b0e2eaf73350d4a2c6b3517b83f9fc5710b
David Chin <davidchin.yf@gmail.com>
2018-03-07 10:09:24 +1100

    refactor(checkout): CHECKOUT-2951 Flatten core folder

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-export { createCheckoutClient, createCheckoutService, createLanguageService } from './core';
+export { default as createCheckoutClient } from './create-checkout-client';
+export { default as createCheckoutService } from './create-checkout-service';
+export { default as createLanguageService } from './locale/create-language-service';
 export { createTimeout } from '@bigcommerce/request-sender';
