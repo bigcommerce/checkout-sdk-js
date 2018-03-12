ad20009cc8c6abaeae8351c1f82003e3f4d4dc61
David Chin <davidchin.yf@gmail.com>
2018-03-12 16:42:03 +1100

    refactor(cart): CHECKOUT-2954 Add InternalCart mapper

---
+++ src/cart/map-to-internal-line-item.ts
@@ -0,0 +1,22 @@
+import { LineItem } from './line-item';
+import InternalLineItem from './internal-line-item';
+
+export default function mapToInternalLineItem(item: LineItem, existingItem: InternalLineItem, type: string): InternalLineItem {
+    return {
+        amount: existingItem.amount,
+        amountAfterDiscount: existingItem.amountAfterDiscount,
+        attributes: existingItem.attributes,
+        discount: item.discountAmount,
+        id: item.id,
+        imageUrl: item.imageUrl,
+        integerAmount: existingItem.integerAmount,
+        integerAmountAfterDiscount: existingItem.integerAmountAfterDiscount,
+        integerDiscount: existingItem.integerDiscount,
+        integerTax: existingItem.integerTax,
+        name: item.name,
+        quantity: item.quantity,
+        tax: existingItem.tax,
+        variantId: item.variantId,
+        type,
+    };
+}
---
+++ src/cart/map-to-internal-line-items.ts
@@ -0,0 +1,33 @@
+import { LineItem } from './line-item';
+import { find } from 'lodash';
+import InternalLineItem from './internal-line-item';
+import LineItemMap from './line-item-map';
+import mapToInternalLineItem from './map-to-internal-line-item';
+
+export default function mapToInternalLineItems(itemMap: LineItemMap, existingItems: InternalLineItem[]): InternalLineItem[] {
+    return (Object.keys(itemMap) as Array<keyof LineItemMap>)
+        .reduce((result, key) => [
+            ...result,
+            ...(itemMap[key] as LineItem[]).map((item) => {
+                const existingItem = find(existingItems, { id: item.id })!;
+
+                return mapToInternalLineItem(item, existingItem, mapToInternalLineItemType(key));
+            }),
+        ], [] as InternalLineItem[]);
+}
+
+function mapToInternalLineItemType(type: string): string {
+    switch (type) {
+        case 'physicalItems':
+            return 'ItemPhysicalEntity';
+
+        case 'digitalItems':
+            return 'ItemDigitalEntity';
+
+        case 'giftCertificates':
+            return 'ItemGiftCertificateEntity';
+
+        default:
+            return '';
+    }
+}
