2c1001608c0bfde28e88b1d7e490121c838a662b
David Chin <davidchin.yf@gmail.com>
2018-03-12 16:38:59 +1100

    refactor(checkout): CHECKOUT-2954 Add InternalAddress mapper

--- src/address/index.ts
+++ src/address/index.ts
@@ -1,3 +1,5 @@
 export { default as Address } from './address';
 export { default as InternalAddress } from './internal-address';
 export { default as isAddressEqual } from './is-address-equal';
+
+export { default as mapToInternalAddress } from './map-to-internal-address';
---
+++ src/address/map-to-internal-address.spec.ts
@@ -0,0 +1,10 @@
+import { getShippingAddress } from '../shipping/shipping-addresses.mock';
+import { getShippingAddress as getInternalShippingAddress } from '../shipping/internal-shipping-addresses.mock';
+import mapToInternalAddress from './map-to-internal-address';
+
+describe('mapToInternalAddress()', () => {
+    it('maps to internal address', () => {
+        expect(mapToInternalAddress(getShippingAddress(), getInternalShippingAddress()))
+            .toEqual(getInternalShippingAddress());
+    });
+});
