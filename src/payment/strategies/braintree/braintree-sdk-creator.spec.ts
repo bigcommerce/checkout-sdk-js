f04085003b772e02049720523580349a5706c5e6
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2018-03-19 08:11:54 +1100

    feat(payment): CHECKOUT-2644 Braintree SDK Creator

---
+++ src/payment/strategies/braintree/braintree-sdk-creator.spec.ts
@@ -0,0 +1,179 @@
+import { NotInitializedError } from '../../../common/error/errors';
+import BraintreeScriptLoader from './braintree-script-loader';
+import BraintreeSDKCreator from './braintree-sdk-creator';
+import {
+    getClientMock,
+    getDataCollectorMock,
+    getModuleCreatorMock,
+    getThreeDSecureMock,
+} from './braintree.mocks';
+
+describe('Braintree SDK Creator', () => {
+    let braintreeScriptLoader: BraintreeScriptLoader;
+    let clientMock;
+    let clientCreatorMock;
+
+    beforeEach(() => {
+        clientMock = getClientMock();
+        clientCreatorMock = getModuleCreatorMock(clientMock);
+        braintreeScriptLoader = {} as BraintreeScriptLoader;
+    });
+
+    describe('#constructor()', () => {
+        it('creates and instance of the sdk Creator', () => {
+            const braintreeSDKCreator = new BraintreeSDKCreator(braintreeScriptLoader);
+            expect(braintreeSDKCreator).toBeInstanceOf(BraintreeSDKCreator);
+        });
+    });
+
+    describe('#getClient()', () => {
+        beforeEach(() => {
+            braintreeScriptLoader.loadClient = jest.fn().mockReturnValue(Promise.resolve(clientCreatorMock));
+        });
+
+        it('uses the right arguments to create the client', async () => {
+            const braintreeSDKCreator = new BraintreeSDKCreator(braintreeScriptLoader);
+            braintreeSDKCreator.initialize('clientToken');
+            const client = await braintreeSDKCreator.getClient();
+            expect(clientCreatorMock.create).toHaveBeenCalledWith({ authorization: 'clientToken' });
+        });
+
+        it('returns a copy of the client', async () => {
+            const braintreeSDKCreator = new BraintreeSDKCreator(braintreeScriptLoader);
+            braintreeSDKCreator.initialize('clientToken');
+            const client = await braintreeSDKCreator.getClient();
+            expect(client).toBe(clientMock);
+        });
+
+        it('always returns the same instance of the client', async () => {
+            const braintreeSDKCreator = new BraintreeSDKCreator(braintreeScriptLoader);
+            braintreeSDKCreator.initialize('clientToken');
+
+            const client1 = await braintreeSDKCreator.getClient();
+            const client2 = await braintreeSDKCreator.getClient();
+
+            expect(braintreeScriptLoader.loadClient).toHaveBeenCalledTimes(1);
+            expect(clientCreatorMock.create).toHaveBeenCalledTimes(1);
+            expect(client1).toBe(client2);
+        });
+
+        it('throws if no client token is provided', async () => {
+            const braintreeSDKCreator = new BraintreeSDKCreator(braintreeScriptLoader);
+            expect(() => braintreeSDKCreator.getClient()).toThrow(expect.any(NotInitializedError));
+        });
+    });
+
+    describe('#get3DS()', () => {
+        let threeDSecureMock;
+        let threeDSecureCreatorMock;
+        let braintreeSDKCreator;
+
+        beforeEach(() => {
+            threeDSecureMock = getThreeDSecureMock();
+            threeDSecureCreatorMock = getModuleCreatorMock(threeDSecureMock);
+            braintreeScriptLoader.load3DS = jest.fn().mockReturnValue(Promise.resolve(threeDSecureCreatorMock));
+            braintreeSDKCreator = new BraintreeSDKCreator(braintreeScriptLoader);
+            jest.spyOn(braintreeSDKCreator, 'getClient').mockReturnValue(Promise.resolve(clientMock));
+        });
+
+        it('returns a promise that resolves to the 3ds client', async () => {
+            const threeDSecure = await braintreeSDKCreator.get3DS();
+
+            expect(threeDSecureCreatorMock.create).toHaveBeenCalledWith({ client: clientMock });
+            expect(threeDSecure).toBe(threeDSecureMock);
+        });
+
+        it('always returns the same instance of the 3ds client', async () => {
+            const threeDSecure1 = await braintreeSDKCreator.get3DS();
+            const threeDSecure2 = await braintreeSDKCreator.get3DS();
+
+            expect(threeDSecure1).toBe(threeDSecure2);
+            expect(braintreeScriptLoader.load3DS).toHaveBeenCalledTimes(1);
+            expect(threeDSecureCreatorMock.create).toHaveBeenCalledTimes(1);
+        });
+
+        it('throws if getting the client throws', async () => {
+            const errorMessage = 'some_error';
+            braintreeSDKCreator.getClient.mockImplementation(() => { throw new Error(errorMessage); });
+
+            expect(() => braintreeSDKCreator.get3DS()).toThrow(errorMessage);
+        });
+    });
+
+    describe('#getDataCollector()', () => {
+        let dataCollectorMock;
+        let dataCollectorCreatorMock;
+        let braintreeSDKCreator;
+
+        beforeEach(() => {
+            dataCollectorMock = getDataCollectorMock();
+            dataCollectorCreatorMock = getModuleCreatorMock(dataCollectorMock);
+            braintreeScriptLoader.loadDataCollector = jest.fn().mockReturnValue(Promise.resolve(dataCollectorCreatorMock));
+            braintreeSDKCreator = new BraintreeSDKCreator(braintreeScriptLoader);
+            jest.spyOn(braintreeSDKCreator, 'getClient').mockReturnValue(Promise.resolve(clientMock));
+        });
+
+        it('uses the right parameters to instanciate a data collector', async () => {
+            const dataCollector = await braintreeSDKCreator.getDataCollector();
+            expect(dataCollectorCreatorMock.create).toHaveBeenCalledWith({ client: clientMock, kount: true });
+        });
+
+        it('always returns the same instance of the 3ds client', async () => {
+            const dataCollector1 = await braintreeSDKCreator.getDataCollector();
+            const dataCollector2 = await braintreeSDKCreator.getDataCollector();
+            expect(dataCollector1).toBe(dataCollector2);
+            expect(braintreeScriptLoader.loadDataCollector).toHaveBeenCalledTimes(1);
+            expect(dataCollectorCreatorMock.create).toHaveBeenCalledTimes(1);
+        });
+
+        it('returns the data collector information', async () => {
+            const dataCollector = await braintreeSDKCreator.getDataCollector();
+            expect(dataCollector).toEqual(expect.objectContaining({ deviceData: 'my_device_session_id' }));
+        });
+
+        it('catches the KOUNT_IS_NOT_ENABLED error ', async () => {
+            dataCollectorCreatorMock.create.mockReturnValue(Promise.reject({ code: 'DATA_COLLECTOR_KOUNT_NOT_ENABLED' }));
+            await expect(braintreeSDKCreator.getDataCollector()).resolves.toEqual(expect.objectContaining({ deviceData: undefined }));
+        });
+
+        it('throws if some other error is returned', async () => {
+            dataCollectorCreatorMock.create.mockReturnValue(Promise.reject({ code: 'OTHER_RANDOM_ERROR' }));
+            await expect(braintreeSDKCreator.getDataCollector()).rejects.toEqual({ code: 'OTHER_RANDOM_ERROR' });
+        });
+    });
+
+    describe('#teardown()', () => {
+        let braintreeSDKCreator;
+        let dataCollectorMock;
+        let threeDSecureMock;
+
+        beforeEach(() => {
+            dataCollectorMock = getDataCollectorMock();
+            threeDSecureMock = getThreeDSecureMock();
+
+            braintreeScriptLoader.loadDataCollector = jest.fn().mockReturnValue(Promise.resolve(getModuleCreatorMock(dataCollectorMock)));
+            braintreeScriptLoader.load3DS = jest.fn().mockReturnValue(Promise.resolve(getModuleCreatorMock(threeDSecureMock)));
+            braintreeScriptLoader.loadClient = jest.fn().mockReturnValue(Promise.resolve(getModuleCreatorMock(clientMock)));
+
+            braintreeSDKCreator = new BraintreeSDKCreator(braintreeScriptLoader);
+            braintreeSDKCreator.initialize('clientToken');
+        });
+
+        it('calls teardown in all the dependencies', async () => {
+            await braintreeSDKCreator.get3DS();
+            await braintreeSDKCreator.getDataCollector();
+
+            await braintreeSDKCreator.teardown();
+            expect(dataCollectorMock.teardown).toHaveBeenCalled();
+            expect(threeDSecureMock.teardown).toHaveBeenCalled();
+        });
+
+        it('only teardown instanciated dependencies', async () => {
+            await braintreeSDKCreator.getDataCollector();
+
+            await braintreeSDKCreator.teardown();
+            expect(dataCollectorMock.teardown).toHaveBeenCalled();
+            expect(threeDSecureMock.teardown).not.toHaveBeenCalled();
+        });
+    });
+});
---
+++ src/payment/strategies/braintree/braintree-sdk-creator.ts
@@ -0,0 +1,79 @@
+import { NotInitializedError } from '../../../common/error/errors';
+
+import { Braintree } from './braintree';
+import BraintreeScriptLoader from './braintree-script-loader';
+
+export default class BraintreeSDKCreator {
+    private _client?: Promise<Braintree.Client>;
+    private _3ds?: Promise<Braintree.ThreeDSecure>;
+    private _dataCollector?: Promise<Braintree.DataCollector>;
+    private _clientToken?: string;
+
+    constructor(
+        private _braintreeScriptLoader: BraintreeScriptLoader
+    ) {}
+
+    initialize(clientToken: string) {
+        this._clientToken = clientToken;
+    }
+
+    getClient(): Promise<Braintree.Client> {
+        if (!this._clientToken) {
+            throw new NotInitializedError();
+        }
+
+        if (!this._client) {
+            this._client = this._braintreeScriptLoader.loadClient()
+                .then(client => client.create({ authorization: this._clientToken }));
+        }
+
+        return this._client;
+    }
+
+    get3DS(): Promise<Braintree.ThreeDSecure> {
+        if (!this._3ds) {
+            this._3ds = Promise.all([
+                this.getClient(),
+                this._braintreeScriptLoader.load3DS(),
+            ])
+            .then(([client, threeDSecure]) => threeDSecure.create({ client }));
+        }
+
+        return this._3ds;
+    }
+
+    getDataCollector(): Promise<Braintree.DataCollector> {
+        if (!this._dataCollector) {
+            this._dataCollector = Promise.all([
+                this.getClient(),
+                this._braintreeScriptLoader.loadDataCollector(),
+            ])
+            .then(([client, dataCollector]) => dataCollector.create({ client, kount: true }))
+            .catch((error) => {
+                if (error && error.code === 'DATA_COLLECTOR_KOUNT_NOT_ENABLED') {
+                    return { deviceData: undefined, teardown: () => Promise.resolve() };
+                }
+
+                throw error;
+            });
+        }
+
+        return this._dataCollector;
+    }
+
+    teardown(): Promise<void> {
+        return Promise.all([
+            this._teardown(this._3ds),
+            this._teardown(this._dataCollector),
+        ]).then(() => {
+            this._3ds = undefined;
+            this._dataCollector = undefined;
+        });
+    }
+
+    private _teardown(module?: Promise<Braintree.Module>) {
+        return module ?
+            module.then((mod) => mod.teardown()) :
+            Promise.resolve();
+    }
+}
