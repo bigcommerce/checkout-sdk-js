c344e9bc4154728e5f97aebf040b95753b37dd4c
David Chin <davidchin.yf@gmail.com>
2018-03-29 22:29:48 +1100

    fix(payment): CHECKOUT-3007 Fix issue where shoppers cannot submit offsite payment

--- src/payment/payment-action-creator.spec.js
+++ src/payment/payment-action-creator.spec.js
@@ -10,6 +10,7 @@ describe('PaymentActionCreator', () => {
 
     beforeEach(() => {
         paymentRequestSender = {
+            initializeOffsitePayment: jest.fn(() => Promise.resolve()),
             submitPayment: jest.fn(() => Promise.resolve(getResponse(getPaymentResponseBody()))),
         };
 
@@ -56,6 +57,42 @@ describe('PaymentActionCreator', () => {
                         },
                     ]);
                 });
+        });
+    });
+
+    describe('#initializeOffsitePayment()', () => {
+        it('dispatches actions to data store', async () => {
+            const actions = await paymentActionCreator.initializeOffsitePayment(getPayment())
+                .toArray()
+                .toPromise();
+
+            expect(actions).toEqual([
+                { type: actionTypes.INITIALIZE_OFFSITE_PAYMENT_REQUESTED },
+                { type: actionTypes.INITIALIZE_OFFSITE_PAYMENT_SUCCEEDED },
+            ]);
+        });
+
+        it('dispatches error actions to data store if unsuccessful', async () => {
+            jest.spyOn(paymentRequestSender, 'initializeOffsitePayment').mockReturnValue(
+                Promise.reject()
+            );
+
+            const errorHandler = jest.fn((action) => Observable.of(action));
+            const actions = await paymentActionCreator.initializeOffsitePayment(getPayment())
+                .catch(errorHandler)
+                .toArray()
+                .toPromise();
+
+            expect(errorHandler).toHaveBeenCalled();
+            expect(actions).toEqual([
+                {
+                    type: actionTypes.INITIALIZE_OFFSITE_PAYMENT_REQUESTED,
+                },
+                {
+                    type: actionTypes.INITIALIZE_OFFSITE_PAYMENT_FAILED,
+                    error: true,
+                },
+            ]);
         });
     });
 });
