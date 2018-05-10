018dc26e1bbc3cc547eb37abd71696a4403f4185
Daniel Del Core <daniel.delcore.92@gmail.com>
2018-05-10 11:32:16 +1000

    refactor(payment): PAYMENTS-2909 Move store querying to intrument action creator

--- src/payment/instrument/instrument-action-creator.ts
+++ src/payment/instrument/instrument-action-creator.ts
@@ -1,126 +1,127 @@
-import { createAction, createErrorAction, Action } from '@bigcommerce/data-store';
+import { createAction, createErrorAction, Action, ThunkAction } from '@bigcommerce/data-store';
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
 
+import { InternalCheckoutSelectors, ReadableCheckoutStore } from '../../checkout';
 import { addMinutes, isFuture } from '../../common/date-time';
+import { MissingDataError } from '../../common/error/errors';
 
+import Instrument, { InstrumentRequestContext, VaultAccessToken } from './instrument';
 import * as actionTypes from './instrument-action-types';
 import InstrumentRequestSender from './instrument-request-sender';
 
-/**
- * @todo Convert this file into TypeScript properly
- */
 export default class InstrumentActionCreator {
     constructor(
         private _instrumentRequestSender: InstrumentRequestSender
     ) {}
 
-    /**
-     * @param {string} storeId
-     * @param {number} shopperId
-     * @param {?VaultAccessToken} accessToken
-     * @return {Observable<Action>}
-     */
-    loadInstruments(storeId: string, shopperId: number, accessToken: any): Observable<Action> {
-        return Observable.create((observer: Observer<Action>) => {
-            observer.next(createAction(actionTypes.LOAD_INSTRUMENTS_REQUESTED));
+    loadInstruments(): ThunkAction<Action, InternalCheckoutSelectors> {
+        return store =>
+            Observable.create((observer: Observer<Action>) => {
+                observer.next(createAction(actionTypes.LOAD_INSTRUMENTS_REQUESTED));
 
-            this._getValidAccessToken(accessToken)
-                .then(currentToken =>
-                    this._instrumentRequestSender.getInstruments(storeId, shopperId, currentToken.vaultAccessToken)
-                        .then(({ body }) => {
-                            observer.next(createAction(actionTypes.LOAD_INSTRUMENTS_SUCCEEDED, body, currentToken));
-                            observer.complete();
-                        })
-                )
-                .catch(response => {
-                    observer.error(createErrorAction(actionTypes.LOAD_INSTRUMENTS_FAILED, response));
-                });
-        });
+                const { token, storeId, customerId } = this._getInstrumentState(store);
+
+                return this._getValidAccessToken(token)
+                    .then(currentToken =>
+                        this._instrumentRequestSender.getInstruments(storeId, customerId, currentToken.vaultAccessToken)
+                            .then(({ body }) => {
+                                observer.next(createAction(actionTypes.LOAD_INSTRUMENTS_SUCCEEDED, body, currentToken));
+                                observer.complete();
+                            })
+                    )
+                    .catch(response => {
+                        observer.error(createErrorAction(actionTypes.LOAD_INSTRUMENTS_FAILED, response));
+                    });
+            });
     }
 
-    /**
-     * @param {string} storeId
-     * @param {string} shopperId
-     * @param {?VaultAccessToken} accessToken
-     * @param {InstrumentRequestBody} instrument
-     * @return {Observable<Action>}
-     */
-    vaultInstrument(storeId: string, shopperId: number, accessToken: any, instrument: any): Observable<Action> {
-        return Observable.create((observer: Observer<Action>) => {
-            observer.next(createAction(actionTypes.VAULT_INSTRUMENT_REQUESTED));
+    vaultInstrument(instrument: Instrument): ThunkAction<Action, InternalCheckoutSelectors> {
+        return store =>
+            Observable.create((observer: Observer<Action>) => {
+                observer.next(createAction(actionTypes.VAULT_INSTRUMENT_REQUESTED));
 
-            this._getValidAccessToken(accessToken)
-                .then(currentToken =>
-                    this._instrumentRequestSender.vaultInstrument(storeId, shopperId, currentToken.vaultAccessToken, instrument)
-                        .then(({ body }) => {
-                            observer.next(createAction(actionTypes.VAULT_INSTRUMENT_SUCCEEDED, body, currentToken));
-                            observer.complete();
-                        })
-                )
-                .catch(response => {
-                    observer.error(createErrorAction(actionTypes.VAULT_INSTRUMENT_FAILED, response));
-                });
-        });
+                const { token, storeId, customerId } = this._getInstrumentState(store);
+
+                return this._getValidAccessToken(token)
+                    .then(currentToken =>
+                        this._instrumentRequestSender.vaultInstrument(storeId, customerId, instrument, currentToken.vaultAccessToken)
+                            .then(({ body }) => {
+                                observer.next(createAction(actionTypes.VAULT_INSTRUMENT_SUCCEEDED, body, currentToken));
+                                observer.complete();
+                            })
+                    )
+                    .catch(response => {
+                        observer.error(createErrorAction(actionTypes.VAULT_INSTRUMENT_FAILED, response));
+                    });
+            });
     }
 
-    /**
-     * @param {string} storeId
-     * @param {string} shopperId
-     * @param {?VaultAccessToken} accessToken
-     * @param {string} instrumentId
-     * @return {Observable<Action>}
-     */
-    deleteInstrument(storeId: string, shopperId: number, accessToken: any, instrumentId: string): Observable<Action> {
-        return Observable.create((observer: Observer<Action>) => {
-            observer.next(createAction(actionTypes.DELETE_INSTRUMENT_REQUESTED, undefined, { instrumentId }));
+    deleteInstrument(instrumentId: string): ThunkAction<Action, InternalCheckoutSelectors> {
+        return store =>
+            Observable.create((observer: Observer<Action>) => {
+                observer.next(createAction(actionTypes.DELETE_INSTRUMENT_REQUESTED, undefined, { instrumentId }));
 
-            this._getValidAccessToken(accessToken)
-                .then(currentToken =>
-                    this._instrumentRequestSender.deleteInstrument(storeId, shopperId, currentToken.vaultAccessToken, instrumentId)
-                        .then(() => {
-                            observer.next(createAction(actionTypes.DELETE_INSTRUMENT_SUCCEEDED, undefined, {
-                                instrumentId,
-                                ...currentToken,
-                            }));
-                            observer.complete();
-                        })
-                )
-                .catch(response => {
-                    observer.error(createErrorAction(actionTypes.DELETE_INSTRUMENT_FAILED, response, { instrumentId }));
-                });
-        });
+                const { token, storeId, customerId } = this._getInstrumentState(store);
+
+                return this._getValidAccessToken(token)
+                    .then(currentToken =>
+                        this._instrumentRequestSender.deleteInstrument(storeId, customerId, currentToken.vaultAccessToken, instrumentId)
+                            .then(() => {
+                                observer.next(createAction(actionTypes.DELETE_INSTRUMENT_SUCCEEDED, undefined, {
+                                    instrumentId,
+                                    ...currentToken,
+                                }));
+                                observer.complete();
+                            })
+                    )
+                    .catch(response => {
+                        observer.error(createErrorAction(actionTypes.DELETE_INSTRUMENT_FAILED, response, { instrumentId }));
+                    });
+            });
     }
 
-    /**
-     * @private
-     * @param {VaultAccessToken} accessToken
-     * @return {boolean}
-     */
-    private _isValidVaultAccessToken(accessToken: any): boolean {
-        if (!accessToken || !accessToken.vaultAccessToken) {
+    private _isValidVaultAccessToken(token: VaultAccessToken): boolean {
+        if (!token || !token.vaultAccessToken) {
             return false;
         }
 
         const expiryBuffer = 2;
-        const expiry = addMinutes(new Date(accessToken.vaultAccessExpiry), expiryBuffer);
+        const expiry = addMinutes(new Date(token.vaultAccessExpiry), expiryBuffer);
 
         return isFuture(expiry);
     }
 
-    /**
-     * Requests a new vault access token if one is not supplied
-     * @private
-     * @param {VaultAccessToken} [accessToken]
-     * @return {Promise<VaultAccessToken>}
-     */
-    private _getValidAccessToken(accessToken: any): Promise<any> {
-        return this._isValidVaultAccessToken(accessToken)
-            ? Promise.resolve(accessToken)
+    private _getValidAccessToken(token: VaultAccessToken): Promise<VaultAccessToken> {
+        return this._isValidVaultAccessToken(token)
+            ? Promise.resolve(token)
             : this._instrumentRequestSender.getVaultAccessToken()
                 .then(({ body = {} }: any) => ({
                     vaultAccessToken: body.data.token,
                     vaultAccessExpiry: body.data.expires_at,
                 }));
+    }
+
+    private _getInstrumentState(store: ReadableCheckoutStore): InstrumentRequestContext {
+        const state = store.getState();
+        const config = state.config.getStoreConfig();
+        const customer = state.customer.getCustomer();
+
+        if (!config || !customer) {
+            throw new MissingDataError('Unable to proceed because "config" or "customer" data is missing.');
+        }
+
+        const { customerId } = customer;
+        const { storeId } = config.storeProfile;
+        const { vaultAccessToken = null, vaultAccessExpiry = null } = state.instruments.getInstrumentsMeta() || {};
+
+        return {
+            customerId,
+            storeId,
+            token: {
+                vaultAccessToken,
+                vaultAccessExpiry,
+            },
+        };
     }
 }
