5270d59f953c09aeb8bc5b67e6d4d6e2652b4283
David Chin <davidchin.yf@gmail.com>
2018-06-05 16:36:31 +1000

    docs(common): CHECKOUT-3060 Add documentation for parameters

--- src/common/http-request/request-options.ts
+++ src/common/http-request/request-options.ts
@@ -1,5 +1,13 @@
 import { Timeout } from '@bigcommerce/request-sender';
 
+/**
+ * A set of options for configuring an asynchronous request.
+ */
 export default interface RequestOptions {
+    /**
+     * Provide this option if you want to cancel or time out the request. If the
+     * timeout object completes before the request, the request will be
+     * cancelled.
+     */
     timeout?: Timeout;
 }
--- src/customer/customer-request-options.ts
+++ src/customer/customer-request-options.ts
@@ -2,11 +2,37 @@ import { RequestOptions } from '../common/http-request';
 
 import { AmazonPayCustomerInitializeOptions, BraintreeVisaCheckoutCustomerInitializeOptions } from './strategies';
 
+/**
+ * A set of options for configuring any requests related to the customer step of
+ * the current checkout flow.
+ *
+ * Some payment methods have their own sign-in or sign-out flow. Therefore, you
+ * need to indicate the method you want to use if you need to trigger a specific
+ * flow for signing in or out a customer. Otherwise, these options are not required.
+ */
 export interface CustomerRequestOptions extends RequestOptions {
     methodId?: string;
 }
 
+/**
+ * A set of options that are required to initialize the customer step of the
+ * current checkout flow.
+ *
+ * Some payment methods have specific requirements for setting the customer
+ * details for checkout. For example, Amazon Pay requires the customer to sign in
+ * using their sign-in button. As a result, you may need to provide additional
+ * information in order to initialize the customer step of checkout.
+ */
 export interface CustomerInitializeOptions extends CustomerRequestOptions {
+    /**
+     * The options that are required to initialize the customer step of checkout
+     * when using Amazon Pay.
+     */
     amazon?: AmazonPayCustomerInitializeOptions;
+
+    /**
+     * The options that are required to initialize the customer step of checkout
+     * when using Visa Checkout provided by Braintree.
+     */
     braintreevisacheckout?: BraintreeVisaCheckoutCustomerInitializeOptions;
 }
