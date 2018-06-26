988fcd1f59b099f23ff49951b27adbeab0f5147b
Luis Sanchez <luis.sanchez@bigcommerce.com>
2018-06-26 12:13:30 +1000

    fix(shipping): CHECKOUT-3183 Return shipping address in quote using shipping selector

--- src/quote/map-to-internal-quote.ts
+++ src/quote/map-to-internal-quote.ts
@@ -1,15 +1,15 @@
-import { mapToInternalAddress, InternalAddress } from '../address';
+import { mapToInternalAddress, Address, InternalAddress } from '../address';
 import { Checkout } from '../checkout';
 
 import InternalQuote from './internal-quote';
 
-export default function mapToInternalQuote(checkout: Checkout): InternalQuote {
+export default function mapToInternalQuote(checkout: Checkout, shippingAddress?: Address): InternalQuote {
     const consignment = checkout.consignments && checkout.consignments[0];
 
     return {
         orderComment: checkout.customerMessage,
         shippingOption: consignment && consignment.selectedShippingOption ? consignment.selectedShippingOption.id : undefined,
         billingAddress: checkout.billingAddress ? mapToInternalAddress(checkout.billingAddress) : {} as InternalAddress,
-        shippingAddress: consignment ? mapToInternalAddress(checkout.consignments[0].shippingAddress, checkout.consignments[0].id) : undefined,
+        shippingAddress: shippingAddress && mapToInternalAddress(shippingAddress),
     };
 }
