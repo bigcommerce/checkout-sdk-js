9bc034faa9b6b534d90993a624b3110ad7a76a52
Luis Sanchez <luis.sanchez@bigcommerce.com>
2018-06-26 12:43:27 +1000

    fix(shipping): CHECKOUT-3253 Return selected shipping option as available shipping options

--- src/shipping/map-to-internal-shipping-options.spec.ts
+++ src/shipping/map-to-internal-shipping-options.spec.ts
@@ -1,10 +1,17 @@
+import { omit } from 'lodash';
+
 import { getConsignment } from './consignments.mock';
-import { getShippingOptions as getInternalShippingOptions } from './internal-shipping-options.mock';
+import { getFlatRateOption, getShippingOptions as getInternalShippingOptions } from './internal-shipping-options.mock';
 import mapToInternalShippingOptions from './map-to-internal-shipping-options';
 
 describe('mapToInternalShippingOptions()', () => {
     it('maps to internal shipping options', () => {
-        expect(mapToInternalShippingOptions([getConsignment()], getInternalShippingOptions()))
+        expect(mapToInternalShippingOptions([getConsignment()]))
+            .toEqual(getInternalShippingOptions());
+    });
+
+    it('maps to selected shipping option if none available', () => {
+        expect(mapToInternalShippingOptions([omit(getConsignment(), 'availableShippingOptions')]))
             .toEqual(getInternalShippingOptions());
     });
 });
