729fbe488a94376d4fefc7c75c46f10c92973263
David Chin <davidchin.yf@gmail.com>
2018-10-17 14:53:58 +1100

    refactor(embedded-checkout): CHECKOUT-3660 Create no-op EmbeddedCheckoutMessenger if not inside iframe

--- src/embedded-checkout/iframe-content/create-embedded-checkout-messenger.spec.ts
+++ src/embedded-checkout/iframe-content/create-embedded-checkout-messenger.spec.ts
@@ -1,9 +1,15 @@
 import createEmbeddedCheckoutMessenger from './create-embedded-checkout-messenger';
-import EmbeddedCheckoutMessenger from './embedded-checkout-messenger';
+import IframeEmbeddedCheckoutMessenger from './iframe-embedded-checkout-messenger';
+import NoopEmbeddedCheckoutMessenger from './noop-embedded-checkout-messenger';
 
 describe('createEmbeddedCheckoutMessenger()', () => {
-    it('creates embedded checkout messenger', () => {
+    it('creates no-op embedded checkout messenger if not inside iframe', () => {
         expect(createEmbeddedCheckoutMessenger({ parentOrigin: 'https://foobar.mybigcommerece.com' }))
-            .toBeInstanceOf(EmbeddedCheckoutMessenger);
+            .toBeInstanceOf(NoopEmbeddedCheckoutMessenger);
+    });
+
+    it('creates embedded checkout messenger if inside iframe', () => {
+        expect(createEmbeddedCheckoutMessenger({ parentOrigin: 'https://foobar.mybigcommerece.com', parentWindow: Object.create(window) }))
+            .toBeInstanceOf(IframeEmbeddedCheckoutMessenger);
     });
 });
