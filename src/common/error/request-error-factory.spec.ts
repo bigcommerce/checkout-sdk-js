0530ada78c0ecad9ca7ee75d70034ea2504d73ff
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2018-12-10 10:55:24 +1100

    fix(payment): CHECKOUT-3797 Surface transaction_declined error message

--- src/common/error/request-error-factory.spec.ts
+++ src/common/error/request-error-factory.spec.ts
@@ -1,8 +1,9 @@
-import { merge } from 'lodash';
+import { first, merge, without } from 'lodash';
 
-import { getErrorResponse, getTimeoutResponse } from '../http-request/responses.mock';
+import { getErrorResponse, getErrorResponseBody, getTimeoutResponse } from '../http-request/responses.mock';
 
 import { RequestError, TimeoutError, UnrecoverableError } from './errors';
+import { PAYMENT_ERROR_CODES } from './errors/map-from-payment-error-response';
 import RequestErrorFactory from './request-error-factory';
 
 describe('RequestErrorFactory', () => {
@@ -45,5 +46,20 @@ describe('RequestErrorFactory', () => {
 
         expect(error).toBeInstanceOf(TimeoutError);
         expect((error as TimeoutError).status).toEqual(0);
+    });
+
+    it('creates `RequestError` for alternative payment error codes and maps the message correctly', () => {
+        const factory = new RequestErrorFactory();
+        const errorResponse = getErrorResponseBody({
+            errors: [{
+                code: first(without(PAYMENT_ERROR_CODES, 'payment')),
+                message: 'foo bar',
+            }],
+        });
+
+        const error = factory.createError(getErrorResponse(errorResponse));
+
+        expect(error).toBeInstanceOf(RequestError);
+        expect(error.message).toBe('foo bar');
     });
 });
