62a110c013096d72318edcfe6265d83360cde33a
Carlos Lopez <clopez@intersysconsulting.com>
2019-08-05 15:50:52 -0500

    feat(payment): INT-1721 Update unit testing and coverage

--- src/payment/strategies/adyenv2/adyenv2-script-loader.ts
+++ src/payment/strategies/adyenv2/adyenv2-script-loader.ts
@@ -5,44 +5,52 @@ import { StandardError } from '../../../common/error/errors';
 import { AdyenClient, AdyenConfiguration, AdyenHostWindow } from './adyenv2';
 
 export default class AdyenV2ScriptLoader {
+    private _stylesheets: { [key: string]: Promise<Event> } = {};
+
     constructor(
         private _scriptLoader: ScriptLoader,
         private _window: AdyenHostWindow = window
     ) {}
 
     load(configuration: AdyenConfiguration): Promise<AdyenClient> {
-        this._loadStylesheet(`https://checkoutshopper-${configuration.environment}.adyen.com/checkoutshopper/sdk/3.0.0/adyen.css`);
+        return this._loadStylesheet(
+            `https://checkoutshopper-${configuration.environment}.adyen.com/checkoutshopper/sdk/3.0.0/adyen.css`
+        ).then(() => {
+            return this._scriptLoader
+                .loadScript(`https://checkoutshopper-${configuration.environment}.adyen.com/checkoutshopper/sdk/3.0.0/adyen.js`)
+                .then(() => {
+                    if (!this._window.AdyenCheckout) {
+                        throw new StandardError();
+                    }
 
-        return this._scriptLoader
-            .loadScript(`https://checkoutshopper-${configuration.environment}.adyen.com/checkoutshopper/sdk/3.0.0/adyen.js`)
-            .then(() => {
-                if (!this._window.AdyenCheckout) {
-                    throw new StandardError();
-                }
+                    return new this._window.AdyenCheckout(configuration);
+                });
+        }).catch(() => {
+            throw new StandardError();
+        });
 
-                return new this._window.AdyenCheckout(configuration);
-            });
     }
 
     private _loadStylesheet(src: string): Promise<Event> {
-        const _stylesheets: { [key: string]: Promise<Event> } = {};
-​
-        if (!_stylesheets[src]) {
-            _stylesheets[src] = new Promise((resolve, reject) => {
-                const stylesheet = document.createElement('link') as HTMLLinkElement;
-​
-                stylesheet.onload = event => resolve(event);
+        if (!this._stylesheets[src]) {
+            this._stylesheets[src] = new Promise((resolve, reject) => {
+                const stylesheet = document.createElement('link');
+
+                stylesheet.onload = event => {
+                    resolve(event);
+                };
                 stylesheet.onerror = event => {
-                    delete _stylesheets[src];
+                    delete this._stylesheets[src];
                     reject(event);
                 };
+                stylesheet.type = 'text/css';
                 stylesheet.rel = 'stylesheet';
                 stylesheet.href = src;
-​
+
                 document.head.appendChild(stylesheet);
             });
         }
-​
-        return _stylesheets[src];
+
+        return this._stylesheets[src];
     }
 }
