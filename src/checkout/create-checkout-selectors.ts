f7b3ba5334e938364c879a4262677d1a2ef0cae4
David Chin <davidchin.yf@gmail.com>
2019-08-05 11:28:22 +1000

    perf(checkout): CHECKOUT-4272 Add function for creating checkout selectors factory

--- src/checkout/create-checkout-selectors.ts
+++ src/checkout/create-checkout-selectors.ts
@@ -6,18 +6,26 @@ import {
     InternalCheckoutSelectors,
 } from '../checkout';
 
-export default function createCheckoutSelectors(selectors: InternalCheckoutSelectors): CheckoutSelectors {
+export type CheckoutSelectorsFactory = (selectors: InternalCheckoutSelectors) => CheckoutSelectors;
+
+export function createCheckoutSelectorsFactory(): CheckoutSelectorsFactory {
     const createCheckoutStoreSelector = createCheckoutStoreSelectorFactory();
     const createCheckoutStoreErrorSelector = createCheckoutStoreErrorSelectorFactory();
     const createCheckoutStoreStatusSelector = createCheckoutStoreStatusSelectorFactory();
 
-    const data = createCheckoutStoreSelector(selectors);
-    const errors = createCheckoutStoreErrorSelector(selectors);
-    const statuses = createCheckoutStoreStatusSelector(selectors);
+    return (selectors: InternalCheckoutSelectors) => {
+        const data = createCheckoutStoreSelector(selectors);
+        const errors = createCheckoutStoreErrorSelector(selectors);
+        const statuses = createCheckoutStoreStatusSelector(selectors);
 
-    return {
-        data,
-        errors,
-        statuses,
+        return {
+            data,
+            errors,
+            statuses,
+        };
     };
+}
+
+export default function createCheckoutSelectors(selectors: InternalCheckoutSelectors): CheckoutSelectors {
+    return createCheckoutSelectorsFactory()(selectors);
 }
