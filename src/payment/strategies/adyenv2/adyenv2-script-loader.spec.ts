096cb8922a878d8beae4b5f8ca71ea3ee674e8f9
Carlos Lopez <clopez@intersysconsulting.com>
2019-08-05 15:50:52 -0500

    feat(payment): INT-1721 Update unit testing and coverage

--- src/payment/strategies/adyenv2/adyenv2-script-loader.spec.ts
+++ src/payment/strategies/adyenv2/adyenv2-script-loader.spec.ts
@@ -1,68 +1,96 @@
 import { ScriptLoader } from '@bigcommerce/script-loader';
 
-​
 import { StandardError } from '../../../common/error/errors';
 
 import { AdyenConfiguration, AdyenHostWindow } from './adyenv2';
 import AdyenV2ScriptLoader from './adyenv2-script-loader';
 import { getAdyenClient, getAdyenConfiguration } from './adyenv2.mock';
-​
+
 describe('AdyenV2ScriptLoader', () => {
     let adyenV2ScriptLoader: AdyenV2ScriptLoader;
     let scriptLoader: ScriptLoader;
     let mockWindow: AdyenHostWindow;
-​
+
     beforeEach(() => {
         mockWindow = { } as AdyenHostWindow;
         scriptLoader = {} as ScriptLoader;
         adyenV2ScriptLoader = new AdyenV2ScriptLoader(scriptLoader, mockWindow);
     });
-​
+
     describe('#load()', () => {
         const adyenClient = getAdyenClient();
-​
         const configuration = getAdyenConfiguration();
-​
+        const stylesheet = document.createElement('link');
+
         beforeEach(() => {
             scriptLoader.loadScript = jest.fn(() => {
                 mockWindow.AdyenCheckout = jest.fn(
                     (configuration: AdyenConfiguration) => adyenClient
                 );
-​
+
                 return Promise.resolve();
             });
-​
-            const stylesheet = document.createElement('link') as HTMLLinkElement;
-​
+
+            stylesheet.type = 'text/css';
+            stylesheet.rel = 'stylesheet';
+            stylesheet.href = 'https://checkoutshopper-test.adyen.com/checkoutshopper/sdk/3.0.0/adyen.css';
+
             jest.spyOn(document, 'createElement')
                 .mockImplementation(() => stylesheet);
-​
-            jest.spyOn(document.head, 'appendChild').mockReturnValue(Promise.resolve());
+
+            jest.spyOn(document.head, 'appendChild')
+                .mockImplementation(element => {
+                    element.onload(new Event('onload'));
+                });
         });
-​
+
         afterEach(() => {
             jest.restoreAllMocks();
         });
-​
-        it('loads the JS', async () => {
+
+        it('loads the JS and CSS', async () => {
             await adyenV2ScriptLoader.load(configuration);
-​
-            expect(scriptLoader.loadScript).toHaveBeenCalledWith(`https://checkoutshopper-test.adyen.com/checkoutshopper/sdk/3.0.0/adyen.js`);
+
+            expect(scriptLoader.loadScript)
+                .toHaveBeenCalledWith(`https://checkoutshopper-test.adyen.com/checkoutshopper/sdk/3.0.0/adyen.js`);
+            expect(document.head.appendChild).toHaveBeenCalledWith(stylesheet);
         });
-​
+
+        it('loads the JS and returns the existing CSS', async () => {
+            await adyenV2ScriptLoader.load(configuration);
+            await adyenV2ScriptLoader.load(configuration);
+
+            expect(scriptLoader.loadScript)
+                .toHaveBeenCalledWith(`https://checkoutshopper-test.adyen.com/checkoutshopper/sdk/3.0.0/adyen.js`);
+            expect(document.head.appendChild).toHaveBeenCalledWith(stylesheet);
+        });
+
         it('returns the JS from the window', async () => {
             const adyenJs = await adyenV2ScriptLoader.load(configuration);
-​
+
             expect(adyenJs).toBe(adyenClient);
         });
-​
+
         it('throws an error when window is not set', async () => {
             scriptLoader.loadScript = jest.fn(() => {
                 mockWindow.AdyenCheckout = undefined;
-​
+
                 return Promise.resolve();
             });
-​
+
+            try {
+                await adyenV2ScriptLoader.load(configuration);
+            } catch (error) {
+                expect(error).toBeInstanceOf(StandardError);
+            }
+        });
+
+        it('throws an error when stylesheet is not loaded', async () => {
+            jest.spyOn(document.head, 'appendChild')
+                .mockImplementation(element => {
+                    element.onerror(new Event('onerror'));
+                });
+
             try {
                 await adyenV2ScriptLoader.load(configuration);
             } catch (error) {
