d264abfc1756a48f4d682c37103ffa8bddecfb22
AlanMunozN <amunoz@intersysconsulting.com>
2020-03-26 16:48:03 -0600

    feat(payment): INT-2452 Add billing and shipping for klarna

--- src/payment/strategies/klarnav2/index.ts
+++ src/payment/strategies/klarnav2/index.ts
@@ -2,3 +2,4 @@ export { default as KlarnaV2PaymentStrategy } from './klarnav2-payment-strategy'
 export { default as KlarnaV2PaymentInitializeOptions } from './klarnav2-payment-initialize-options';
 export { default as KlarnaPayments, KlarnaLoadResponse } from './klarna-payments';
 export { default as KlarnaV2ScriptLoader } from './klarnav2-script-loader';
+export { supportedCountries, supportedCountriesRequiringStates } from './klarna-supported-countries';
