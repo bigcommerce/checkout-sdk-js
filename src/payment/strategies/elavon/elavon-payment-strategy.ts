370779ddaef26dd79f302646d17af57772538883
christianahvilla <christianahvilla@gmail.com>
2020-06-19 08:22:43 -0500

    IP

--- src/payment/strategies/elavon/elavon-payment-strategy.ts
+++ src/payment/strategies/elavon/elavon-payment-strategy.ts
@@ -3,8 +3,9 @@ import { HostedFormFactory } from '../../../hosted-form';
 import { OrderActionCreator, OrderRequestBody } from '../../../order';
 import { PaymentArgumentInvalidError } from '../../errors';
 import PaymentActionCreator from '../../payment-action-creator';
-import { PaymentRequestOptions } from '../../payment-request-options';
 import { CreditCardPaymentStrategy } from '../credit-card';
+
+import { PaymentRequestOptions } from './../../payment-request-options';
 
 export default class ElavonPaymentStrategy extends CreditCardPaymentStrategy {
     constructor(
@@ -21,28 +22,32 @@ export default class ElavonPaymentStrategy extends CreditCardPaymentStrategy {
         );
     }
 
-    async execute(orderRequest: OrderRequestBody, options?: PaymentRequestOptions): Promise<InternalCheckoutSelectors> {
-        const { payment, payment: { methodId = '' } = {}, ...order } = orderRequest;
-        const { paymentMethods: { getPaymentMethodOrThrow } } = this._store.getState();
-        const paymentMethod = getPaymentMethodOrThrow(methodId);
-        const ip = paymentMethod.initializationData.ip;
+    execute(orderRequest: OrderRequestBody, options?: PaymentRequestOptions): Promise<InternalCheckoutSelectors> {
+        const { payment } = orderRequest;
+        const paymentData = payment && payment.paymentData;
 
         if (!payment) {
             throw new PaymentArgumentInvalidError(['payment']);
         }
 
+        // isVaulted
+
+        const { paymentMethods: { getPaymentMethodOrThrow } } = this._store.getState();
+        const paymentMethod = getPaymentMethodOrThrow(payment.methodId);
+        const ip = paymentMethod.initializationData.ip;
+
         return this._store.dispatch(
-            this._orderActionCreator.submitOrder(order, options)
+            this._orderActionCreator.submitOrder(orderRequest, options)
         )
         .then( () =>
             this._store.dispatch(this._paymentActionCreator.submitPayment({
                 ...payment,
-                methodId: payment.methodId,
-                    paymentData: {
-                        formattedPayload: {
-                            ip_address: ip,
-                        },
+                paymentData: {
+                    ...paymentData,
+                    formattedPayload: {
+                        ip_address: ip,
                     },
+                },
             }))
         )
         .catch((error: Error) => this._handleError(error));
