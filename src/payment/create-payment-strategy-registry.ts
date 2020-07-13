c2b21a90d174c253d179412561ba2b96a67d0eb5
David Chin <davidchin.yf@gmail.com>
2020-07-13 12:29:55 +1000

    fix(payment): CHECKOUT-5025 Fix Elavon Converge strategy so that it can utilise hosted payment form

--- src/payment/create-payment-strategy-registry.ts
+++ src/payment/create-payment-strategy-registry.ts
@@ -473,6 +473,7 @@ export default function createPaymentStrategyRegistry(
             store,
             orderActionCreator,
             paymentActionCreator,
+            hostedFormFactory,
             formPoster
         )
     );
--- src/payment/strategies/converge/converge-payment-strategy.ts
+++ src/payment/strategies/converge/converge-payment-strategy.ts
@@ -3,34 +3,27 @@ import { some } from 'lodash';
 
 import { CheckoutStore, InternalCheckoutSelectors } from '../../../checkout';
 import { RequestError } from '../../../common/error/errors';
+import { HostedFormFactory } from '../../../hosted-form';
 import { OrderActionCreator, OrderRequestBody } from '../../../order';
 import { OrderFinalizationNotRequiredError } from '../../../order/errors';
-import { PaymentArgumentInvalidError } from '../../errors';
 import PaymentActionCreator from '../../payment-action-creator';
 import { PaymentRequestOptions } from '../../payment-request-options';
 import * as paymentStatusTypes from '../../payment-status-types';
-import PaymentStrategy from '../payment-strategy';
+import { CreditCardPaymentStrategy } from '../credit-card';
 
-export default class ConvergePaymentStrategy implements PaymentStrategy {
+export default class ConvergePaymentStrategy extends CreditCardPaymentStrategy {
     constructor(
-        private _store: CheckoutStore,
-        private _orderActionCreator: OrderActionCreator,
-        private _paymentActionCreator: PaymentActionCreator,
+        store: CheckoutStore,
+        orderActionCreator: OrderActionCreator,
+        paymentActionCreator: PaymentActionCreator,
+        hostedFormFactory: HostedFormFactory,
         private _formPoster: FormPoster
-    ) {}
+    ) {
+        super(store, orderActionCreator, paymentActionCreator, hostedFormFactory);
+    }
 
     execute(payload: OrderRequestBody, options?: PaymentRequestOptions): Promise<InternalCheckoutSelectors> {
-        const { payment, ...order } = payload;
-        const paymentData = payment && payment.paymentData;
-
-        if (!payment || !paymentData) {
-            throw new PaymentArgumentInvalidError(['payment.paymentData']);
-        }
-
-        return this._store.dispatch(this._orderActionCreator.submitOrder(order, options))
-            .then(() =>
-                this._store.dispatch(this._paymentActionCreator.submitPayment({ ...payment, paymentData }))
-            )
+        return super.execute(payload, options)
             .catch(error => {
                 if (!(error instanceof RequestError) || !some(error.body.errors, { code: 'three_d_secure_required' })) {
                     return Promise.reject(error);
@@ -55,13 +48,5 @@ export default class ConvergePaymentStrategy implements PaymentStrategy {
         }
 
         return Promise.reject(new OrderFinalizationNotRequiredError());
-    }
-
-    initialize(): Promise<InternalCheckoutSelectors> {
-        return Promise.resolve(this._store.getState());
-    }
-
-    deinitialize(): Promise<InternalCheckoutSelectors> {
-        return Promise.resolve(this._store.getState());
     }
 }
