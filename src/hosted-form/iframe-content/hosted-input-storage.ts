05154279c82293c04f246ffacdaf760ca6de449b
David Chin <davidchin.yf@gmail.com>
2021-01-28 18:33:30 +1100

    fix(payment): CHECKOUT-5324 Set nonce in memory instead of local storage

--- src/hosted-form/iframe-content/hosted-input-storage.ts
+++ src/hosted-form/iframe-content/hosted-input-storage.ts
@@ -1,15 +1,11 @@
-import { BrowserStorage } from '../../common/storage';
-
 export default class HostedInputStorage {
-    constructor(
-        private _storage: BrowserStorage
-    ) {}
+    private _nonce?: string;
 
     setNonce(nonce: string): void {
-        this._storage.setItem('nonce', nonce);
+        this._nonce = nonce;
     }
 
-    getNonce(): string | null {
-        return this._storage.getItem('nonce');
+    getNonce(): string | undefined {
+        return this._nonce;
     }
 }
