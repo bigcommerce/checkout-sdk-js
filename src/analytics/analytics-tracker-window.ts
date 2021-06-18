0e490efe6326785bde5261772063510201b63e90
leprik <oleg.guienok@bigcommerce.com>
2021-06-18 17:04:33 +0300

    fix(checkout): DATA-7883 Fix Segment + GAEE issue

--- src/analytics/analytics-tracker-window.ts
+++ src/analytics/analytics-tracker-window.ts
@@ -1,7 +1,5 @@
 export interface AnalyticsTracker {
     track(step: string, data: any): void;
-    hit(hitName: string, data: any): void;
-    hasPayloadLimit(data: any): boolean;
 }
 
 export default interface AnalyticsTrackerWindow extends Window {
