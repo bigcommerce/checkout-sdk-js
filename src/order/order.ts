06ccfb40e1c8df9f641ab2b4b03368df57849eb1
FranciscoOnt <fontiveros@apexsystems.com>
2021-08-20 17:05:54 -0500

    feat(order): INT-4776 Create a new field for the mandate reference ID

--- src/order/order.ts
+++ src/order/order.ts
@@ -32,7 +32,6 @@ export default interface Order {
     status: string;
     taxes: Tax[];
     taxTotal: number;
-    mandateUrl?: string;
 }
 
 export type OrderPayments = Array<GatewayOrderPayment | GiftCertificateOrderPayment>;
@@ -42,6 +41,7 @@ export type OrderMeta = OrderMetaState;
 export interface OrderPayment {
     providerId: string;
     gatewayId?: string;
+    methodId?: string;
     paymentId?: string;
     description: string;
     amount: number;
@@ -51,6 +51,10 @@ export interface GatewayOrderPayment extends OrderPayment {
     detail: {
         step: string;
         instructions: string;
+    };
+    mandate?: {
+        id: string;
+        url?: string;
     };
 }
 
