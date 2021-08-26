1a51c9b219efa63f8967b9c3df8ce84b71f70552
Peng <88361607+bc-peng@users.noreply.github.com>
2021-08-26 10:06:57 +1000

    fix(checkout): JIRA-5757 Fix Sign Out Button Not Working on Checkout when Persistent Cart is Enabled

--- src/config/config-request-sender.ts
+++ src/config/config-request-sender.ts
@@ -3,6 +3,7 @@ import { RequestSender, Response } from '@bigcommerce/request-sender';
 import { ContentType, INTERNAL_USE_ONLY, RequestOptions } from '../common/http-request';
 
 import Config from './config';
+import {CheckoutNotAvailableError} from "../checkout/errors";
 
 export default class ConfigRequestSender {
     constructor(
@@ -12,12 +13,17 @@ export default class ConfigRequestSender {
     loadConfig({ timeout }: RequestOptions = {}): Promise<Response<Config>> {
         const url = '/api/storefront/checkout-settings';
 
-        return this._requestSender.get(url, {
+        return this._requestSender.get<Config>(url, {
             timeout,
             headers: {
                 Accept: ContentType.JsonV1,
                 'X-API-INTERNAL': INTERNAL_USE_ONLY,
             },
+        }).catch(error => {
+            if (error.status >= 400 && error.status < 500) {
+                throw new CheckoutNotAvailableError(error);
+            }
+            throw error;
         });
     }
 }
