6e4c14a504b4c520fc24773f4f4d322f26129ebe
Peng <88361607+bc-peng@users.noreply.github.com>
2021-08-26 12:24:04 +1000

    fix(checkout): JIRA-5757 Fix Sign Out Button Not Working on Checkout …

--- src/config/config-request-sender.ts
+++ src/config/config-request-sender.ts
@@ -1,5 +1,6 @@
 import { RequestSender, Response } from '@bigcommerce/request-sender';
 
+import { CheckoutNotAvailableError } from '../checkout/errors';
 import { ContentType, INTERNAL_USE_ONLY, RequestOptions } from '../common/http-request';
 
 import Config from './config';
@@ -12,12 +13,17 @@ export default class ConfigRequestSender {
     loadConfig({ timeout }: RequestOptions = {}): Promise<Response<Config>> {
         const url = '/api/storefront/checkout-settings';
 
-        return this._requestSender.get(url, {
+        return this._requestSender.get<Config>(url, {
             timeout,
             headers: {
                 Accept: ContentType.JsonV1,
                 'X-API-INTERNAL': INTERNAL_USE_ONLY,
             },
+        }).catch(error => {
+            if (error.status >= 400 && error.status < 500) {
+                throw new CheckoutNotAvailableError(error);
+            }
+            throw error;
         });
     }
 }
