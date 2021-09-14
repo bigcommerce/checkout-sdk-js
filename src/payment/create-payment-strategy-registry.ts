d8e982e005183822b0e11148f0393f7a9d3eb144
orecio <64860381+orecio-bc@users.noreply.github.com>
2021-09-13 23:46:42 -0500

    fix(payment): INT-4810 Forget checkout provider and reload payment methods - Clearpay (#1234)

--- src/payment/create-payment-strategy-registry.ts
+++ src/payment/create-payment-strategy-registry.ts
@@ -240,6 +240,7 @@ export default function createPaymentStrategyRegistry(
             orderActionCreator,
             paymentActionCreator,
             paymentMethodActionCreator,
+            remoteCheckoutRequestSender,
             storeCreditActionCreator,
             new ClearpayScriptLoader(scriptLoader)
         )
