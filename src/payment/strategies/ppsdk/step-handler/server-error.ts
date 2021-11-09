26e5510a533042fca51619a40928b34325aea689
Lee Dunkley <56807262+leeBigCommerce@users.noreply.github.com>
2021-11-09 11:28:20 +1100

    chore(payment): PAYMENTS-7409 remove unreachable, duplicate request error handling from PPSDK strategy (#1283)

--- src/payment/strategies/ppsdk/step-handler/server-error.ts
+++
@@ -1,8 +0,0 @@
-import { RequestError } from '../../../../common/error/errors';
-import { PaymentsAPIResponse } from '../ppsdk-payments-api-response';
-
-export const isServerError = (status: number): boolean =>
-    status < 200 || status >= 400;
-
-export const handleServerError = (response: PaymentsAPIResponse) =>
-    Promise.reject(new RequestError(response));
