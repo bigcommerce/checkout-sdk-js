893f1fca9e86a257be23b3803fdec91f291d884c
Carlos Enrique Lopez Hernandez <35146660+clopezh@users.noreply.github.com>
2021-12-06 16:48:37 -0600

    feat(payment): INT-5000 Throw error when payment method isn't available (#1286)

---
+++ src/payment/strategies/digitalriver/digitalriver-error.ts
@@ -0,0 +1,12 @@
+import { StandardError } from '../../../common/error/errors';
+
+const defaultMessage: string = 'There was an error while processing your payment. Please try again or contact us.';
+
+export default class DigitalRiverError extends StandardError {
+    constructor(type: string, name: string, message?: string) {
+        super(message || defaultMessage);
+
+        this.type = type;
+        this.name = name;
+    }
+}
