f58a3c381ef595298d974f01006048dca0965abb
Diego Gomez <dieinmort@gmail.com>
2022-02-21 13:55:36 -0600

    feat(payment): INT-5156 Klarna add support for poland, portugal, ireland

--- src/payment/strategies/klarnav2/klarna-supported-countries.ts
+++ src/payment/strategies/klarnav2/klarna-supported-countries.ts
@@ -1,2 +1,2 @@
-export const supportedCountries = ['AT', 'BE', 'CA', 'CH', 'DE', 'DK', 'ES', 'FI', 'FR', 'GB', 'IT', 'NL', 'NO', 'NZ', 'SE'];
+export const supportedCountries = ['AT', 'BE', 'CA', 'CH', 'DE', 'DK', 'ES', 'FI', 'FR', 'GB', 'IE', 'IT', 'NL', 'NO', 'NZ', 'PL', 'PT', 'SE'];
 export const supportedCountriesRequiringStates = ['AU'];
