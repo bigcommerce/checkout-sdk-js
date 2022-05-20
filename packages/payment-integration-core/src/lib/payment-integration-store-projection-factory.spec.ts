e8e53bb3a7f05dd52f7be2bc8eb2fc38163bcd16
animesh1987 <animeshsingh1987@gmail.com>
2022-05-20 15:24:35 +1000

    chore(common): CHECKOUT-6377 Move test files

---
+++ packages/payment-integration-core/src/lib/payment-integration-store-projection-factory.spec.ts
@@ -0,0 +1,41 @@
+import { ReadableDataStore } from '@bigcommerce/data-store';
+import { createCheckoutStore, InternalCheckoutSelectors } from '@bigcommerce/checkout-sdk/core';
+
+import createPaymentIntegrationSelectors from './create-payment-integration-selectors';
+import PaymentIntegrationSelectors from './payment-integration-selectors';
+import PaymentIntegrationStoreProjectionFactory from './payment-integration-store-projection-factory';
+
+describe('PaymentIntegrationStoreProjectionFactory', () => {
+    let subject: PaymentIntegrationStoreProjectionFactory;
+    let store: ReadableDataStore<InternalCheckoutSelectors>;
+    let selectors: PaymentIntegrationSelectors;
+    let transformer: (selectors: InternalCheckoutSelectors) => PaymentIntegrationSelectors;
+
+    beforeEach(() => {
+        store = createCheckoutStore();
+        selectors = createPaymentIntegrationSelectors(store.getState());
+        transformer = jest.fn(() => selectors);
+        subject = new PaymentIntegrationStoreProjectionFactory(transformer);
+    });
+
+    describe('#create', () => {
+        it('returns data store projection that returns payment integration selectors', () => {
+            const output = subject.create(store);
+
+            expect(output.getState())
+                .toEqual(selectors);
+        });
+
+        it('returns data store projection that notifies subscribers with payment integration selectors', () => {
+            const store = createCheckoutStore();
+            const output = subject.create(store);
+            const subscriber = jest.fn();
+
+            output.subscribe(subscriber);
+            output.notifyState();
+
+            expect(subscriber)
+                .toHaveBeenCalledWith(selectors);
+        });
+    });
+});
