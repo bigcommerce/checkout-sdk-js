f9be21c4109b7c3bd40fca06994d1c0e5f36db27
animesh1987 <animeshsingh1987@gmail.com>
2022-08-13 00:35:00 +1000

    chore(common): CHECKOUT-000 Use workspace.json as package src

--- package-loader-rule.js
+++ package-loader-rule.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { projects } = require('./workspace.json');
 
 const packages = [
     'core',
@@ -6,14 +7,16 @@ const packages = [
     'payment-integration',
 ];
 
-const getPackagesWithPath = packages.map(package => {
-    const packageSrcPath =  path.join(__dirname, `packages/${package}/src`);
+const tsSrcPackages = [];
 
-    return {
+for (const [, packagePath] of Object.entries(projects)) {
+    const packageSrcPath =  path.join(__dirname, `${packagePath}/src`);
+
+    tsSrcPackages.push({
         test: /\.[tj]s$/,
         include: packageSrcPath,
         loader: 'ts-loader',
-    };
-});
+    });
+}
 
-module.exports = getPackagesWithPath;
+module.exports = tsSrcPackages;
