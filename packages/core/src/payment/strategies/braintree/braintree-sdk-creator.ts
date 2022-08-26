b012ea28958b5524717ee624c17df407f877cbae
Andrii <andrii.vitvitskyi@bigcommerce.com>
2022-08-26 11:32:36 +0300

    feat(payment): PAYPAL-1507 test

--- packages/core/src/payment/strategies/braintree/braintree-sdk-creator.ts
+++ packages/core/src/payment/strategies/braintree/braintree-sdk-creator.ts
@@ -72,7 +72,6 @@ export default class BraintreeSDKCreator {
             const paypalSdkLoadConfig = {
                 currency: config.currency,
                 components: PAYPAL_COMPONENTS.toString(),
-                'enable-funding':['paylater'],
             };
 
             if (!this._window.paypal) {
