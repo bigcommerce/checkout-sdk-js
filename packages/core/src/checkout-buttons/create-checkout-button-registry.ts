bb6b2429287b1f1fc5c9ad02893a6c6a307aedfc
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2022-10-05 09:38:13 +0300

    feat(payment): PAYPAL-1628 added Buy Now implementation for PayPalCommerce alternative methods button strategy (#1624)

--- packages/core/src/checkout-buttons/create-checkout-button-registry.ts
+++ packages/core/src/checkout-buttons/create-checkout-button-registry.ts
@@ -283,6 +283,7 @@ export default function createCheckoutButtonRegistry(
         new PaypalCommerceAlternativeMethodsButtonStrategy(
             store,
             checkoutActionCreator,
+            cartRequestSender,
             formPoster,
             paypalScriptLoader,
             paypalCommerceRequestSender
