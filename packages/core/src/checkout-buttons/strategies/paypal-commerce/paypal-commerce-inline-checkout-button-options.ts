66597b01d7487c5e3d2c7fbeaf70bf4050249f31
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2022-11-08 13:09:58 +0200

    refactor(payment): PAYPAL-1773 removed unnecessary business logic from PayPalCommerceInlineCheckoutButton strategy (#1672)

--- packages/core/src/checkout-buttons/strategies/paypal-commerce/paypal-commerce-inline-checkout-button-options.ts
+++ packages/core/src/checkout-buttons/strategies/paypal-commerce/paypal-commerce-inline-checkout-button-options.ts
@@ -2,32 +2,10 @@ import { PaypalButtonStyleOptions } from '../../../payment/strategies/paypal-com
 
 export interface PaypalCommerceInlineCheckoutButtonInitializeOptions {
     /**
-     * Accelerated Checkout Buttons container - is a generic container for all AC buttons
-     * Used as a container where the button will be rendered with its own container
-     * Example: 'data-cart-accelerated-checkout-buttons'
-     * Info: we are using data attributes as an identifier because the buttons can be rendered in several places on the page
-     */
-    acceleratedCheckoutContainerDataId: string;
-
-    /**
-     * A container identifier what used to add special class for container where the button will be generated in
-     * Example: 'data-paypal-commerce-inline-button'
-     * Info: we are using data attributes as an identifier because the buttons can be rendered in several places on the page
-     */
-    buttonContainerDataId: string;
-
-    /**
      * A class name used to add special class for container where the button will be generated in
      * Default: 'PaypalCommerceInlineButton'
      */
     buttonContainerClassName?: string;
-
-    /**
-     * Used by Accelerated Checkout strategy to hide native action button before rendering PayPal inline checkout button
-     * Example: 'data-checkout-now-button'
-     * Info: we are using data attributes as an identifier because the buttons can be rendered in several places on the page
-     */
-    nativeCheckoutButtonDataId: string;
 
     /**
      * A set of styling options for the checkout button.
@@ -38,4 +16,9 @@ export interface PaypalCommerceInlineCheckoutButtonInitializeOptions {
      * A callback that gets called when payment complete on paypal side.
      */
     onComplete(): void;
+
+    /**
+     * A callback that gets called on any error
+     */
+    onError?(): void;
 }
