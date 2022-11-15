fa797c805acab2b829435dcb3228f5aa9e7644b0
animesh1987 <animeshsingh1987@gmail.com>
2022-11-15 22:31:41 +1100

    chore(common): CHECKOUT-7058 Fix issues in hosted components

--- packages/core/src/hosted-form/hosted-form.ts
+++ packages/core/src/hosted-form/hosted-form.ts
@@ -62,19 +62,21 @@ export default class HostedForm {
         });
     }
 
-    async submit(payload: OrderPaymentRequestBody, additionalActionData?: PaymentAdditionalAction): Promise<HostedInputSubmitSuccessEvent> {
+    async submit(payload: OrderPaymentRequestBody, additionalActionData?: PaymentAdditionalAction): Promise<HostedInputSubmitSuccessEvent | void> {
         try {
             return await this._getFirstField().submitForm(
                 this._fields.map(field => field.getType()),
                 this._payloadTransformer.transform(payload, additionalActionData)
             );
         } catch (error) {
-            const additionalAction = await this._paymentHumanVerificationHandler.handle(error);
+            if (error instanceof Error) {
+                const additionalAction = await this._paymentHumanVerificationHandler.handle(error);
 
-            return await this._getFirstField().submitForm(
-                this._fields.map(field => field.getType()),
-                this._payloadTransformer.transform(payload, additionalAction)
-            );
+                return await this._getFirstField().submitForm(
+                    this._fields.map(field => field.getType()),
+                    this._payloadTransformer.transform(payload, additionalAction)
+                );
+            }
         }
     }
 
