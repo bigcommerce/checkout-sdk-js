16fa0c4b78498dc74fb68edbece0db38d6479069
animesh1987 <animeshsingh1987@gmail.com>
2022-11-25 09:53:19 +1100

    chore(common): CHECKOUT-7058 Fix exports for checkout button

--- packages/core/src/bundles/checkout-button.ts
+++ packages/core/src/bundles/checkout-button.ts
@@ -1,3 +1,19 @@
 export { createTimeout } from '@bigcommerce/request-sender';
 
-export { createCheckoutButtonInitializer } from '../checkout-buttons';
+export { createCheckoutButtonInitializer,
+    AmazonPayV2ButtonInitializeOptions,
+    ApplePayButtonInitializeOptions,
+    BraintreePaypalButtonInitializeOptions, BraintreePaypalCreditButtonInitializeOptions, BraintreeVenmoButtonInitializeOptions,
+    GooglePayButtonInitializeOptions,
+    PaypalButtonInitializeOptions,
+    PaypalCommerceAlternativeMethodsButtonOptions, PaypalCommerceButtonInitializeOptions, PaypalCommerceCreditButtonInitializeOptions, PaypalCommerceInlineCheckoutButtonInitializeOptions, PaypalCommerceVenmoButtonInitializeOptions,
+    BaseCheckoutButtonInitializeOptions,
+    CheckoutButtonInitializeOptions,
+    CheckoutButtonOptions,
+    CheckoutButtonSelectors,
+    CheckoutButtonInitializer,
+    CheckoutButtonInitializerOptions } from '../checkout-buttons';
+
+export { default as CheckoutButtonMethodType } from '../generated/checkout-button-method-type';
+export { default as CheckoutButtonErrorSelector } from '../checkout-buttons/checkout-button-error-selector';
+export { default as CheckoutButtonStatusSelector } from '../checkout-buttons/checkout-button-status-selector';
--- packages/core/src/checkout-buttons/index.ts
+++ packages/core/src/checkout-buttons/index.ts
@@ -1,6 +1,17 @@
 export { default as createCheckoutButtonInitializer } from './create-checkout-button-initializer';
 export { default as checkoutButtonReducer } from './checkout-button-reducer';
 export { default as CheckoutButtonSelector, CheckoutButtonSelectorFactory, createCheckoutButtonSelectorFactory } from './checkout-button-selector';
+export { default as CheckoutButtonSelectors } from './checkout-button-selectors';
 export { default as CheckoutButtonState } from './checkout-button-state';
 export { CheckoutButtonStrategy, CheckoutButtonMethodType } from './strategies';
 export { BaseCheckoutButtonInitializeOptions, CheckoutButtonOptions, CheckoutButtonInitializeOptions } from './checkout-button-options';
+export { default as CheckoutButtonInitializerOptions } from './checkout-button-initializer-options';
+export { default as CheckoutButtonInitializer } from './checkout-button-initializer';
+
+// TODO: This list should be automatically created.
+export { AmazonPayV2ButtonInitializeOptions } from './strategies/amazon-pay-v2';
+export { ApplePayButtonInitializeOptions } from './strategies/apple-pay';
+export { BraintreePaypalButtonInitializeOptions, BraintreePaypalCreditButtonInitializeOptions, BraintreeVenmoButtonInitializeOptions } from './strategies/braintree';
+export { GooglePayButtonInitializeOptions } from './strategies/googlepay';
+export { PaypalButtonInitializeOptions } from './strategies/paypal';
+export { PaypalCommerceAlternativeMethodsButtonOptions, PaypalCommerceButtonInitializeOptions, PaypalCommerceCreditButtonInitializeOptions, PaypalCommerceInlineCheckoutButtonInitializeOptions, PaypalCommerceVenmoButtonInitializeOptions } from './strategies/paypal-commerce';
