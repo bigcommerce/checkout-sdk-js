c03c3761a507156352e1b9b7f7c867ef4093ae83
Andrii <andrii.vitvitskyi@bigcommerce.com>
2022-12-06 14:53:06 +0200

    feat(payment): PAYPAL-1634 test

--- packages/payment-integration-api/src/cart/index.ts
+++ packages/payment-integration-api/src/cart/index.ts
@@ -3,4 +3,3 @@ export { default as InternalLineItem } from './internal-line-item';
 export { PhysicalItem, DigitalItem, GiftCertificateItem } from './line-item';
 export { default as LineItemMap } from './line-item-map';
 export { default as BuyNowCartRequestBody } from './buyNowCart';
-
