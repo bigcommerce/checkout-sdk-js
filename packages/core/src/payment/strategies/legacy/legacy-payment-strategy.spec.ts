23613547554bda6f6537a84a755b0645de35bc29
Animesh Singh <animeshsingh1987@gmail.com>
2022-12-19 18:14:58 +1100

    Revert "chore(common): CHECKOUT-6401 Move legacy payment strategy to separate package (#1727)"
    This reverts commit e946565d2b122687411e03b6507519d6fa5d870a.

---
+++ packages/core/src/payment/strategies/legacy/legacy-payment-strategy.spec.ts
@@ -0,0 +1,66 @@
+import { createAction } from '@bigcommerce/data-store';
+import { createRequestSender } from '@bigcommerce/request-sender';
+import { Observable, of } from 'rxjs';
+
+import {
+    CheckoutRequestSender,
+    CheckoutStore,
+    CheckoutValidator,
+    createCheckoutStore,
+} from '../../../checkout';
+import {
+    OrderActionCreator,
+    OrderActionType,
+    OrderRequestSender,
+    SubmitOrderAction,
+} from '../../../order';
+import { OrderFinalizationNotRequiredError } from '../../../order/errors';
+import { getOrderRequestBody } from '../../../order/internal-orders.mock';
+
+import LegacyPaymentStrategy from './legacy-payment-strategy';
+
+describe('LegacyPaymentStrategy', () => {
+    let orderActionCreator: OrderActionCreator;
+    let store: CheckoutStore;
+    let strategy: LegacyPaymentStrategy;
+    let submitOrderAction: Observable<SubmitOrderAction>;
+
+    beforeEach(() => {
+        store = createCheckoutStore();
+        orderActionCreator = new OrderActionCreator(
+            new OrderRequestSender(createRequestSender()),
+            new CheckoutValidator(new CheckoutRequestSender(createRequestSender())),
+        );
+        submitOrderAction = of(createAction(OrderActionType.SubmitOrderRequested));
+
+        jest.spyOn(orderActionCreator, 'submitOrder').mockReturnValue(submitOrderAction);
+
+        jest.spyOn(store, 'dispatch');
+
+        strategy = new LegacyPaymentStrategy(store, orderActionCreator);
+    });
+
+    it('submits order with payment data', async () => {
+        await strategy.execute(getOrderRequestBody());
+
+        expect(orderActionCreator.submitOrder).toHaveBeenCalledWith(
+            getOrderRequestBody(),
+            undefined,
+        );
+        expect(store.dispatch).toHaveBeenCalledWith(submitOrderAction);
+    });
+
+    it('returns checkout state', async () => {
+        const output = await strategy.execute(getOrderRequestBody());
+
+        expect(output).toEqual(store.getState());
+    });
+
+    it('throws error to inform that order finalization is not required', async () => {
+        try {
+            await strategy.finalize();
+        } catch (error) {
+            expect(error).toBeInstanceOf(OrderFinalizationNotRequiredError);
+        }
+    });
+});
