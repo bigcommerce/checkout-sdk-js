b5fbae47f773b9925a33bb1baa9fca52978e062e
serhii.tkachenko <serhii.tkachenko@bigcommerce.com>
2023-01-11 20:28:18 +0200

    chore(payment): PAYPAL-1904 removed PayPalCommerceCustomerStrategy from core package

--- packages/core/src/customer/strategies/paypalcommerce/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as PaypalCommerceCustomerInitializeOptions } from './paypalcommerce-customer-initialize-options';
-export { default as PaypalCommerceCustomerStrategy } from './paypalcommerce-customer-strategy';
