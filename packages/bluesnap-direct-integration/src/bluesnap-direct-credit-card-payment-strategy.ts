regiapp/sdkbigcommerce

feat(payment): INT-7044 BlueSnapDirect: Complete payment strategy for credit card

packages/bluesnap-direct-integration/src/bluesnap-direct-credit-card-payment-strategy.ts
@@ -1,26 +1,114 @@
 import {
+    CreditCardPaymentStrategy,
+    WithCreditCardPaymentInitializeOptions,
+} from '@bigcommerce/checkout-sdk/credit-card-integration';
+import {
+    guard,
+    HostedFieldType,
+    HostedInputValidateResults,
+    InvalidArgumentError,
+    MissingDataError,
+    MissingDataErrorType,
+    NotInitializedError,
+    NotInitializedErrorType,
+    OrderRequestBody,
+    PaymentArgumentInvalidError,
+    PaymentInitializeOptions,
     PaymentIntegrationService,
-    PaymentStrategy,
+    PaymentRequestOptions,
 } from '@bigcommerce/checkout-sdk/payment-integration-api';
 
-export default class BlueSnapDirectCreditCardPaymentStrategy implements PaymentStrategy {
-    constructor(private _paymentIntegrationService: PaymentIntegrationService) {}
+import BlueSnapDirectHostedForm from './bluesnap-direct-hosted-form';
+import isHostedCardFieldOptionsMap from './is-hosted-card-field-options-map';
 
-    execute(): Promise<void> {
-        return Promise.resolve();
+export default class BlueSnapDirectCreditCardPaymentStrategy extends CreditCardPaymentStrategy {
+    private _paymentFieldsToken?: string;
+
+    constructor(
+        paymentIntegrationService: PaymentIntegrationService,
+        private _blueSnapDirectHostedForm: BlueSnapDirectHostedForm,
+    ) {
+        super(paymentIntegrationService);
     }
 
-    finalize(): Promise<void> {
-        return Promise.resolve();
+    async initialize(
+        options: PaymentInitializeOptions & WithCreditCardPaymentInitializeOptions,
+    ): Promise<void> {
+        const { methodId, gatewayId, creditCard } = options;
+
+        if (!gatewayId || !creditCard) {
+            throw new InvalidArgumentError();
+        }
+
+        const form = { ...creditCard.form };
+
+        if (!isHostedCardFieldOptionsMap(form.fields)) {
+            throw new InvalidArgumentError();
+        }
+
+        Reflect.set(form, 'fields', {
+            [HostedFieldType.CardName]: form.fields[HostedFieldType.CardName],
+        });
+
+        const parentOptions = {
+            ...options,
+            creditCard: {
+                ...creditCard,
+                form: {
+                    ...form,
+                    onValidate: (data: HostedInputValidateResults) =>
+                        this._blueSnapDirectHostedForm.onValidate(data),
+                },
+            },
+        };
+
+        await super.initialize(parentOptions);
+
+        if (!this._shouldRenderHostedForm) {
+            throw new NotInitializedError(NotInitializedErrorType.PaymentNotInitialized);
+        }
+
+        const {
+            config: { testMode },
+            clientToken,
+        } = (
+            await this._paymentIntegrationService.loadPaymentMethod(gatewayId, {
+                params: { method: methodId },
+            })
+        ).getPaymentMethodOrThrow(methodId, gatewayId);
+
+        this._paymentFieldsToken = clientToken;
+
+        await this._blueSnapDirectHostedForm.initialize(testMode);
+        await this._blueSnapDirectHostedForm.attach(this._getPaymentFieldsToken(), creditCard);
     }
 
-    initialize(): Promise<void> {
-        this._paymentIntegrationService.getState();
+    async execute(payload: OrderRequestBody, options?: PaymentRequestOptions): Promise<void> {
+        if (!payload.payment) {
+            throw new PaymentArgumentInvalidError(['payment']);
+        }
 
-        return Promise.resolve();
+        const pfToken = this._getPaymentFieldsToken();
+
+        await this._blueSnapDirectHostedForm.validate().submit();
+        await super.execute(
+            {
+                ...payload,
+                payment: {
+                    ...payload.payment,
+                    paymentData: {
+                        nonce: pfToken,
+                    },
+                },
+            },
+            options,
+        );
     }
 
-    deinitialize(): Promise<void> {
-        return Promise.resolve();
+    private _getPaymentFieldsToken(): string {
+        return guard(
+            this._paymentFieldsToken,
+            () => new MissingDataError(MissingDataErrorType.MissingPaymentToken),
+        );
     }
 }

packages/bluesnap-direct-integration/src/create-bluesnap-direct-credit-card-payment-strategy.ts
@@ -1,0 +1,26 @@
+import { getScriptLoader } from '@bigcommerce/script-loader';
+
+import {
+    PaymentStrategyFactory,
+    toResolvableModule,
+} from '@bigcommerce/checkout-sdk/payment-integration-api';
+
+import BlueSnapDirectCreditCardPaymentStrategy from './bluesnap-direct-credit-card-payment-strategy';
+import BlueSnapDirectHostedForm from './bluesnap-direct-hosted-form';
+import BlueSnapHostedInputValidator from './bluesnap-direct-hosted-input-validator';
+import BlueSnapDirectScriptLoader from './bluesnap-direct-script-loader';
+
+const createBlueSnapDirectCreditCardPaymentStrategy: PaymentStrategyFactory<
+    BlueSnapDirectCreditCardPaymentStrategy
+> = (paymentIntegrationService) =>
+    new BlueSnapDirectCreditCardPaymentStrategy(
+        paymentIntegrationService,
+        new BlueSnapDirectHostedForm(
+            new BlueSnapDirectScriptLoader(getScriptLoader()),
+            new BlueSnapHostedInputValidator(),
+        ),
+    );
+
+export default toResolvableModule(createBlueSnapDirectCreditCardPaymentStrategy, [
+    { id: 'CC', gateway: 'bluesnapdirect' },
+]);

packages/bluesnap-direct-integration/src/mocks/bluesnap-direct-method.mock.ts
@@ -1,0 +1,18 @@
+import { PaymentMethod } from '@bigcommerce/checkout-sdk/payment-integration-api';
+
+export function getBlueSnapDirect(): PaymentMethod {
+    return {
+        clientToken: 'pfToken',
+        config: {
+            cardCode: true,
+            displayName: 'Credit Card',
+            isHostedFormEnabled: true,
+            testMode: true,
+        },
+        gateway: 'bluesnapdirect',
+        id: 'cc',
+        method: 'cc',
+        supportedCards: ['AMEX', 'CUP', 'DINERS', 'DISCOVER', 'JCB', 'MC', 'VISA'],
+        type: 'PAYMENT_TYPE_API',
+    };
+}

bigcommerce/checkout-sdk-js

packages/bluesnap-direct-integration/src/bluesnap-direct-credit-card-payment-strategy.ts
@@ -1,26 +1,91 @@
+import { WithCreditCardPaymentInitializeOptions } from '@bigcommerce/checkout-sdk/credit-card-integration';
 import {
+    guard,
+    InvalidArgumentError,
+    MissingDataError,
+    MissingDataErrorType,
+    OrderFinalizationNotRequiredError,
+    OrderRequestBody,
+    PaymentArgumentInvalidError,
+    PaymentInitializeOptions,
     PaymentIntegrationService,
     PaymentStrategy,
 } from '@bigcommerce/checkout-sdk/payment-integration-api';
 
-export default class BlueSnapDirectCreditCardPaymentStrategy implements PaymentStrategy {
-    constructor(private _paymentIntegrationService: PaymentIntegrationService) {}
+import BlueSnapDirectHostedForm from './bluesnap-direct-hosted-form';
+import isHostedCardFieldOptionsMap from './is-hosted-card-field-options-map';
 
-    execute(): Promise<void> {
-        return Promise.resolve();
+export default class BlueSnapDirectCreditCardPaymentStrategy implements PaymentStrategy {
+    private _paymentFieldsToken?: string;
+
+    constructor(
+        private _paymentIntegrationService: PaymentIntegrationService,
+        private _blueSnapDirectHostedForm: BlueSnapDirectHostedForm,
+    ) {}
+
+    async initialize(
+        options: PaymentInitializeOptions & WithCreditCardPaymentInitializeOptions,
+    ): Promise<void> {
+        const { methodId, gatewayId, creditCard } = options;
+
+        if (!gatewayId || !creditCard || !isHostedCardFieldOptionsMap(creditCard.form.fields)) {
+            throw new InvalidArgumentError();
+        }
+
+        const state = await this._paymentIntegrationService.loadPaymentMethod(gatewayId, {
+            params: { method: methodId },
+        });
+
+        const {
+            config: { testMode },
+            clientToken,
+        } = state.getPaymentMethodOrThrow(methodId, gatewayId);
+
+        this._paymentFieldsToken = clientToken;
+
+        await this._blueSnapDirectHostedForm.initialize(testMode);
+        await this._blueSnapDirectHostedForm.attach(this._getPaymentFieldsToken(), creditCard);
+    }
+
+    async execute(payload: OrderRequestBody): Promise<void> {
+        if (!payload.payment) {
+            throw new PaymentArgumentInvalidError(['payment']);
+        }
+
+        const pfToken = this._getPaymentFieldsToken();
+
+        const { cardHolderName } = await this._blueSnapDirectHostedForm.validate().submit();
+
+        await this._paymentIntegrationService.submitOrder();
+        await this._paymentIntegrationService.submitPayment({
+            ...payload.payment,
+            paymentData: {
+                formattedPayload: {
+                    credit_card_token: {
+                        token: JSON.stringify({
+                            pfToken,
+                            cardHolderName,
+                        }),
+                    },
+                },
+            },
+        });
     }
 
     finalize(): Promise<void> {
-        return Promise.resolve();
-    }
-
-    initialize(): Promise<void> {
-        this._paymentIntegrationService.getState();
-
-        return Promise.resolve();
+        return Promise.reject(new OrderFinalizationNotRequiredError());
     }
 
     deinitialize(): Promise<void> {
+        this._blueSnapDirectHostedForm.detach();
+
         return Promise.resolve();
+    }
+
+    private _getPaymentFieldsToken(): string {
+        return guard(
+            this._paymentFieldsToken,
+            () => new MissingDataError(MissingDataErrorType.MissingPaymentToken),
+        );
     }
 }

packages/bluesnap-direct-integration/src/create-bluesnap-direct-credit-card-payment-strategy.ts
@@ -1,0 +1,28 @@
+import { getScriptLoader } from '@bigcommerce/script-loader';
+
+import {
+    PaymentStrategyFactory,
+    toResolvableModule,
+} from '@bigcommerce/checkout-sdk/payment-integration-api';
+
+import BlueSnapDirectCreditCardPaymentStrategy from './bluesnap-direct-credit-card-payment-strategy';
+import BlueSnapDirectHostedForm from './bluesnap-direct-hosted-form';
+import BlueSnapHostedInputValidator from './bluesnap-direct-hosted-input-validator';
+import BluesnapDirectNameOnCardInput from './bluesnap-direct-name-on-card-input';
+import BlueSnapDirectScriptLoader from './bluesnap-direct-script-loader';
+
+const createBlueSnapDirectCreditCardPaymentStrategy: PaymentStrategyFactory<
+    BlueSnapDirectCreditCardPaymentStrategy
+> = (paymentIntegrationService) =>
+    new BlueSnapDirectCreditCardPaymentStrategy(
+        paymentIntegrationService,
+        new BlueSnapDirectHostedForm(
+            new BlueSnapDirectScriptLoader(getScriptLoader()),
+            new BluesnapDirectNameOnCardInput(),
+            new BlueSnapHostedInputValidator(),
+        ),
+    );
+
+export default toResolvableModule(createBlueSnapDirectCreditCardPaymentStrategy, [
+    { id: 'credit_card', gateway: 'bluesnapdirect' },
+]);

packages/bluesnap-direct-integration/src/mocks/bluesnap-direct-method.mock.ts
@@ -1,0 +1,18 @@
+import { PaymentMethod } from '@bigcommerce/checkout-sdk/payment-integration-api';
+
+export function getBlueSnapDirect(): PaymentMethod {
+    return {
+        clientToken: 'pfToken',
+        config: {
+            cardCode: true,
+            displayName: 'Credit Card',
+            isHostedFormEnabled: true,
+            testMode: true,
+        },
+        gateway: 'bluesnapdirect',
+        id: 'credit_card',
+        method: 'credit_card',
+        supportedCards: ['AMEX', 'CUP', 'DINERS', 'DISCOVER', 'JCB', 'MC', 'VISA'],
+        type: 'PAYMENT_TYPE_API',
+    };
+}