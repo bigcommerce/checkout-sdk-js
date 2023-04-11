e100e34102106b2ad02c08016229415bb955018c
serhii.tkachenko <serhii.tkachenko@bigcommerce.com>
2023-04-11 11:46:17 +0300

    refactor(payment): PAYPAL-000 added billing address mock to payment-integrations-test-utils package

--- packages/payment-integrations-test-utils/src/test-utils/index.ts
+++ packages/payment-integrations-test-utils/src/test-utils/index.ts
@@ -1,5 +1,6 @@
 export { default as PaymentIntegrationServiceMock } from './payment-integration-service.mock';
 export { default as getBuyNowCartRequestBody } from './buy-now-cart-request-body.mock';
+export { default as getBillingAddress } from './billing-address.mock';
 export { default as getCart, getBuyNowCart } from './carts.mock';
 export { default as getCheckout, getCheckoutWithBuyNowCart } from './checkouts.mock';
 export { default as getConfig } from './config.mock';
