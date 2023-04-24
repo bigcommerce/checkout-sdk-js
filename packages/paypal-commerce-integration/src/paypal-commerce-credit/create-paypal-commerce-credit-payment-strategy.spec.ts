e2b437020445831363a7165f53db45b782ae2ffd
serhii.tkachenko <serhii.tkachenko@bigcommerce.com>
2023-04-24 10:56:48 +0300

    refactor(payment): PAYPAL-2366 added PayPalCommerceCredit payment strategy to paypal-commerce-integration package

---
+++ packages/paypal-commerce-integration/src/paypal-commerce-credit/create-paypal-commerce-credit-payment-strategy.spec.ts
@@ -0,0 +1,19 @@
+import { PaymentIntegrationService } from '@bigcommerce/checkout-sdk/payment-integration-api';
+import { PaymentIntegrationServiceMock } from '@bigcommerce/checkout-sdk/payment-integrations-test-utils';
+
+import createPayPalCommerceCreditPaymentStrategy from './create-paypal-commerce-credit-payment-strategy';
+import PayPalCommerceCreditPaymentStrategy from './paypal-commerce-credit-payment-strategy';
+
+describe('createPayPalCommerceCreditPaymentStrategy', () => {
+    let paymentIntegrationService: PaymentIntegrationService;
+
+    beforeEach(() => {
+        paymentIntegrationService = <PaymentIntegrationService>new PaymentIntegrationServiceMock();
+    });
+
+    it('instantiates paypal commerce credit payment strategy', () => {
+        const strategy = createPayPalCommerceCreditPaymentStrategy(paymentIntegrationService);
+
+        expect(strategy).toBeInstanceOf(PayPalCommerceCreditPaymentStrategy);
+    });
+});
