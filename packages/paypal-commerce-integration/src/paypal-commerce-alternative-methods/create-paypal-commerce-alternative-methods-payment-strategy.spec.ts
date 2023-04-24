b2425270818cc011e53868b5b4f331f931888e7a
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2023-04-24 10:23:23 +0300

    refactor(payment): PAYPAL-1894 added PayPalCommerceAlternativeMethodsPaymentStrategy to paypal-commerce-integration package (#1941)

---
+++ packages/paypal-commerce-integration/src/paypal-commerce-alternative-methods/create-paypal-commerce-alternative-methods-payment-strategy.spec.ts
@@ -0,0 +1,20 @@
+import { PaymentIntegrationService } from '@bigcommerce/checkout-sdk/payment-integration-api';
+import { PaymentIntegrationServiceMock } from '@bigcommerce/checkout-sdk/payment-integrations-test-utils';
+
+import createPayPalCommerceAlternativeMethodsPaymentStrategy from './create-paypal-commerce-alternative-methods-payment-strategy';
+import PayPalCommerceAlternativeMethodsPaymentStrategy from './paypal-commerce-alternative-methods-payment-strategy';
+
+describe('createPayPalCommerceAlternativeMethodsPaymentStrategy', () => {
+    let paymentIntegrationService: PaymentIntegrationService;
+
+    beforeEach(() => {
+        paymentIntegrationService = <PaymentIntegrationService>new PaymentIntegrationServiceMock();
+    });
+
+    it('instantiates paypal commerce alternative methods payment strategy', () => {
+        const strategy =
+            createPayPalCommerceAlternativeMethodsPaymentStrategy(paymentIntegrationService);
+
+        expect(strategy).toBeInstanceOf(PayPalCommerceAlternativeMethodsPaymentStrategy);
+    });
+});
