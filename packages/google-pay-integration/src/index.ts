2121e61167466a342d7a1c35f34076a14a3266db
Mauricio Sanchez <mauricio.sanchez@bigcommerce.com>
2023-08-28 13:56:07 -0600

    fix(payment): INT-7676 GooglePay: Add missing export

--- packages/google-pay-integration/src/index.ts
+++ packages/google-pay-integration/src/index.ts
@@ -2,6 +2,7 @@ export { WithGooglePayPaymentInitializeOptions } from './google-pay-payment-init
 
 export { default as createGooglePayAuthorizeNetPaymentStrategy } from './factories/payment/create-google-pay-authorizenet-payment-strategy';
 export { default as createGooglePayCheckoutComPaymentStrategy } from './factories/payment/create-google-pay-checkoutcom-payment-strategy';
+export { default as createGooglePayCybersourcePaymentStrategy } from './factories/payment/create-google-pay-cybersource-payment-strategy';
 export { default as createGooglePayOrbitalPaymentStrategy } from './factories/payment/create-google-pay-orbital-payment-strategy';
 export { default as createGooglePayStripePaymentStrategy } from './factories/payment/create-google-pay-stripe-payment-strategy';
 export { default as createGooglePayWorldpayAccessPaymentStrategy } from './factories/payment/create-google-pay-worldpayaccess-payment-strategy';
