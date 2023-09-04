2677a1dfa1705b8209714f546387b5424ff763b9
bc-nick <nick.tsybulko@bigcommerce.com>
2023-09-04 11:30:01 +0200

    fix(payment): PAYPAL-2843 device data

--- packages/apple-pay-integration/jest.config.js
+++ packages/apple-pay-integration/jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    displayName: "apple-pay",
-    preset: "../../jest.preset.js",
+    displayName: 'apple-pay',
+    preset: '../../jest.preset.js',
     globals: {
-        "ts-jest": {
-            tsconfig: "<rootDir>/tsconfig.spec.json",
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
             diagnostics: false,
         },
     },
-    setupFilesAfterEnv: ["../../jest-setup.js"],
-    coverageDirectory: "../../coverage/packages/apple-pay-integration",
+    setupFilesAfterEnv: ['../../jest-setup.js'],
+    coverageDirectory: '../../coverage/packages/apple-pay-integration',
 };
