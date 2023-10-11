268d51e061283df52f28deec10fa3b5c43afd78a
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2023-10-11 14:26:37 +0300

    fix(payment): PAYPAL-000 added comment to trigger tests build

--- packages/core/src/payment/strategies/braintree/braintree-credit-card-payment-strategy.ts
+++ packages/core/src/payment/strategies/braintree/braintree-credit-card-payment-strategy.ts
@@ -74,6 +74,7 @@ export default class BraintreeCreditCardPaymentStrategy implements PaymentStrate
             this._handleError(error);
         }
 
+        //
         return this._store.getState();
     }
 
