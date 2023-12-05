b823d70bfe4464db970bb394ef120b7f983f6b65
David Chin <davidchin.yf@gmail.com>
2023-12-05 14:49:46 +1100

    feat(checkout): CHECKOUT-9388 Hydrate initial state

--- packages/core/src/checkout/create-checkout-service.ts
+++ packages/core/src/checkout/create-checkout-service.ts
@@ -61,6 +61,7 @@ import { StoreCreditActionCreator, StoreCreditRequestSender } from '../store-cre
 import { SubscriptionsActionCreator, SubscriptionsRequestSender } from '../subscription';
 
 import CheckoutActionCreator from './checkout-action-creator';
+import CheckoutInitialState from './checkout-initial-state';
 import CheckoutRequestSender from './checkout-request-sender';
 import CheckoutService from './checkout-service';
 import CheckoutValidator from './checkout-validator';
@@ -109,7 +110,7 @@ export default function createCheckoutService(options?: CheckoutServiceOptions):
     };
     const { locale = '', shouldWarnMutation = true } = options || {};
     const requestSender = createRequestSender({ host: options && options.host });
-    const store = createCheckoutStore({ config }, { shouldWarnMutation });
+    const store = createCheckoutStore({ config }, options?.initialState, { shouldWarnMutation });
     const paymentClient = createPaymentClient(store);
     const orderRequestSender = new OrderRequestSender(requestSender);
     const checkoutRequestSender = new CheckoutRequestSender(requestSender);
@@ -213,4 +214,5 @@ export interface CheckoutServiceOptions {
     host?: string;
     shouldWarnMutation?: boolean;
     externalSource?: string;
+    initialState?: CheckoutInitialState;
 }
