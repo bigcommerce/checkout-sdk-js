366e323320fcd51a84c149c9691aef5933f7cff0
Vitaliy Koshovyi <vitaliy.koshovyi@bigcommerce.com>
2023-12-12 16:34:58 +0200

    feat(payment): PI-851 blusnap direct ideal strategy creation

--- packages/bluesnap-direct-integration/src/index.ts
+++ packages/bluesnap-direct-integration/src/index.ts
@@ -2,6 +2,7 @@ export { default as createBlueSnapDirectCreditCardPaymentStrategy } from './blue
 export { default as createBlueSnapDirectEcpPaymentStrategy } from './bluesnap-direct-apms/create-bluesnap-direct-ecp-payment-strategy';
 export { default as createBlueSnapDirectAPMPaymentStrategy } from './bluesnap-direct-apms/create-bluesnap-direct-apm-payment-strategy';
 export { default as createBlueSnapDirectSepaPaymentStrategy } from './bluesnap-direct-apms/create-bluesnap-direct-sepa-payment-strategy';
+export { default as createBlueSnapDirectIdealPaymentStrategy } from './bluesnap-direct-apms/create-bluesnap-direct-ideal-payment-strategy';
 export {
     BlueSnapDirectAPMInitializeOptions,
     WithBlueSnapDirectAPMPaymentInitializeOptions,
