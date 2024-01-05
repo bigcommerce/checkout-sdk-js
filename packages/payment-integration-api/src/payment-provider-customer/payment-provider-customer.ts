538ae2f0c31b7cf827f7ebc1b4779e3de72c4a8a
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2024-01-05 13:36:28 +0200

    feat(payment): PAYPAL-3409 updated PaymentProviderCustomer with PPCP AXO related interface

--- packages/payment-integration-api/src/payment-provider-customer/payment-provider-customer.ts
+++ packages/payment-integration-api/src/payment-provider-customer/payment-provider-customer.ts
@@ -1,10 +1,19 @@
+import { AddressRequestBody } from '../address';
 import { CustomerAddress } from '../customer';
 import { CardInstrument } from '../payment';
 
-export type PaymentProviderCustomer = BraintreeAcceleratedCheckoutCustomer;
+export type PaymentProviderCustomer =
+    | BraintreeAcceleratedCheckoutCustomer
+    | PayPalCommerceAcceleratedCheckoutCustomer;
 
 export interface BraintreeAcceleratedCheckoutCustomer {
     authenticationState?: string;
     addresses?: CustomerAddress[];
+    instruments?: CardInstrument[];
+}
+
+export interface PayPalCommerceAcceleratedCheckoutCustomer {
+    authenticationState?: string;
+    addresses?: AddressRequestBody[];
     instruments?: CardInstrument[];
 }
