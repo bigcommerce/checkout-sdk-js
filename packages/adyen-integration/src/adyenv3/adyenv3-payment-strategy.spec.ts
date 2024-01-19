f6e61510250dae179cda6e04d02d4e2cb106ad5a
Vitalii Koshovyi <79574476+vitalii-koshovyi@users.noreply.github.com>
2024-01-19 15:18:34 +0200

    Revert "feat(payment): PI-429 [Adyen] Improve FE validation for APMs on checkout"

--- packages/adyen-integration/src/adyenv3/adyenv3-payment-strategy.spec.ts
+++ packages/adyen-integration/src/adyenv3/adyenv3-payment-strategy.spec.ts
@@ -236,35 +236,6 @@ describe('AdyenV3PaymentStrategy', () => {
                 expect(paymentIntegrationService.submitPayment).toHaveBeenCalledTimes(0);
             });
 
-            it('throws an error when card field is empty', async () => {
-                const adyenInvalidPaymentComponent = {
-                    mount: jest.fn(),
-                    unmount: jest.fn(),
-                    componentRef: {
-                        showValidation: jest.fn(),
-                    },
-                    state: {
-                        data: { 'sepa.ownerName': '' },
-                    },
-                    props: {
-                        type: 'card',
-                    },
-                };
-
-                jest.spyOn(adyenCheckout, 'create').mockReturnValue(adyenInvalidPaymentComponent);
-
-                await strategy.initialize(options);
-
-                await expect(strategy.execute(getOrderRequestBody())).rejects.toThrow(
-                    PaymentInvalidFormError,
-                );
-                expect(
-                    adyenInvalidPaymentComponent.componentRef.showValidation,
-                ).toHaveBeenCalledTimes(1);
-
-                expect(paymentIntegrationService.submitPayment).toHaveBeenCalledTimes(0);
-            });
-
             it('calls submitPayment when paying with vaulted instrument', async () => {
                 jest.spyOn(paymentIntegrationService, 'submitPayment').mockReturnValueOnce(
                     submitPaymentAction,
