5fc10acc3da26706359f425963448ea39bbe0667
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2024-02-28 10:13:09 +0200

    refactor(payment): PAYPAL-3602 updated imports for paypal commerce analytic tracker

---
+++ packages/core/src/analytics/paypal-commerce-analytic-tracker/index.ts
@@ -0,0 +1,2 @@
+export { default as createPayPalCommerceAnalyticTracker } from './create-paypal-commerce-analytic-tracker';
+export { default as PayPalCommerceAnalyticTrackerService } from './paypal-commerce-analytic-tracker-service';
--- packages/core/src/analytics/paypal-commerce-connect-tracker/index.ts
+++
@@ -1,4 +0,0 @@
-// TODO: update createPayPalCommerceConnectTracker with createPayPalCommerceAnalyticTracker
-export { default as createPayPalCommerceConnectTracker } from './create-paypal-commerce-analytic-tracker';
-// TODO: update PayPalCommerceConnectTrackerService with PayPalCommerceAnalyticTrackerService
-export { default as PayPalCommerceConnectTrackerService } from './paypal-commerce-analytic-tracker-service';
