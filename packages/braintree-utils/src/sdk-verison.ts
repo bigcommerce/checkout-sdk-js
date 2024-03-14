b8c54e9e72401e36529df10ef6ec6a4518cabaf3
Andrii <andrii.vitvitskyi@bigcommerce.com>
2024-03-14 17:02:55 +0200

    feat(payment): PAYPAL-0000 revert BT SDK ALPHA version

--- packages/braintree-utils/src/sdk-verison.ts
+++ packages/braintree-utils/src/sdk-verison.ts
@@ -1,2 +1,2 @@
 export const BRAINTREE_SDK_STABLE_VERSION = '3.95.0';
-export const BRAINTREE_SDK_ALPHA_VERSION = '3.97.4-connect-alpha.6.3';
+export const BRAINTREE_SDK_ALPHA_VERSION = '3.95.0-connect-alpha.12';
