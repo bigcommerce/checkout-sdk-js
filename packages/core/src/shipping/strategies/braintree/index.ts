1a8ed0b76359ed0899bc714844fa3bd3111b2b4b
Andrii <andrii.vitvitskyi@bigcommerce.com>
2024-04-26 12:04:38 +0300

    feat(payment): PAYPAL-4069 removed temporary import and export

--- packages/core/src/shipping/strategies/braintree/index.ts
+++ packages/core/src/shipping/strategies/braintree/index.ts
@@ -1,12 +1,2 @@
-// Remove after fastlane naming checkout part merged
-import { default as BraintreeAcceleratedCheckoutInitializeOptions } from './braintree-fastlane-shipping-initialize-options';
-import { default as BraintreeAcceleratedCheckoutShippingStrategy } from './braintree-fastlane-shipping-strategy';
-
 export { default as BraintreeFastlaneInitializeOptions } from './braintree-fastlane-shipping-initialize-options';
 export { default as BraintreeFastlaneShippingStrategy } from './braintree-fastlane-shipping-strategy';
-
-// Remove after fastlane naming checkout part merged
-export {
-    BraintreeAcceleratedCheckoutInitializeOptions,
-    BraintreeAcceleratedCheckoutShippingStrategy,
-};
