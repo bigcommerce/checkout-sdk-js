e1f3262430492c96d3c65252a22bccea239275e0
Peng <peng.zhou@bigcommerce.com>
2024-05-21 12:19:31 +1000

    feat(checkout): CHECKOUT-8284 Create hosted-form package

---
+++ packages/hosted-form/README.md
@@ -0,0 +1,11 @@
+# Hosted Form
+
+This library is for creating a hosted form that can be used outside of the checkout page.
+
+## Running unit tests
+
+Run `nx test hosted-form` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint hosted-form` to execute the lint via [ESLint](https://eslint.org/).
---
+++ packages/hosted-form/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    displayName: 'hosted-form',
+    preset: '../../jest.preset.js',
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+            diagnostics: false,
+        },
+    },
+    setupFilesAfterEnv: ['../../jest-setup.js'],
+    coverageDirectory: '../../coverage/packages/hosted-form',
+};
