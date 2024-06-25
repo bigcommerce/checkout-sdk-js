67ce6596a3e03e1cd32443b381ee87771646c114
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2024-06-25 10:40:58 +0300

    feat(checkout): PAYPAL-4200 provided initial state config for store creation on createCheckoutButtonInitializer (#2533)

--- packages/core/src/checkout-buttons/create-checkout-button-initializer.spec.ts
+++ packages/core/src/checkout-buttons/create-checkout-button-initializer.spec.ts
@@ -1,11 +1,21 @@
 import { createFormPoster } from '@bigcommerce/form-poster';
 import { createRequestSender } from '@bigcommerce/request-sender';
 
+import { createCheckoutStore } from '../checkout';
+
 import CheckoutButtonInitializer from './checkout-button-initializer';
 import createCheckoutButtonInitializer from './create-checkout-button-initializer';
 
 jest.mock('@bigcommerce/form-poster');
 jest.mock('@bigcommerce/request-sender');
+jest.mock('../checkout', () => ({
+    createCheckoutStore: jest.fn(() => ({
+        getState: jest.fn().mockReturnValue({}),
+        subscribe: jest.fn(),
+    })),
+}));
+jest.mock('../payment-integration');
+jest.mock('./create-checkout-button-registry');
 
 describe('createCheckoutButtonInitializer()', () => {
     afterEach(() => {
@@ -21,8 +31,18 @@ describe('createCheckoutButtonInitializer()', () => {
     it('configures instance with host URL', () => {
         const host = 'https://foobar.com';
 
-        createCheckoutButtonInitializer({ host });
+        createCheckoutButtonInitializer({ host, locale: 'en' });
 
+        expect(createCheckoutStore).toHaveBeenCalledWith({
+            config: {
+                meta: {
+                    host,
+                    locale: 'en',
+                },
+                errors: {},
+                statuses: {},
+            },
+        });
         expect(createFormPoster).toHaveBeenCalledWith({ host });
         expect(createRequestSender).toHaveBeenCalledWith({ host });
     });
