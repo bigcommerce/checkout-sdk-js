75ac04b92ecddcd6a603e93563f81d5620b359f6
Sebastian Szafraniec <sebastian.szafraniec@bigcommerce.com>
2024-07-03 15:39:57 +0200

    feat(payment): PI-2284 Add Google Pay on TD Online Mart - checkout customer step

--- packages/google-pay-integration/src/index.ts
+++ packages/google-pay-integration/src/index.ts
@@ -26,6 +26,7 @@ export { default as createGooglePayStripeUpeCustomerStrategy } from './factories
 export { default as createGooglePayWorldpayAccessCustomerStrategy } from './factories/customer/create-google-pay-worldpayaccess-customer-strategy';
 export { default as createGooglePayBraintreeCustomerStrategy } from './factories/customer/create-google-pay-braintree-customer-strategy';
 export { default as createGooglePayPayPalCommerceCustomerStrategy } from './google-pay-paypal-commerce/create-google-pay-paypal-commerce-customer-strategy';
+export { default as createGooglePayTdOnlineMartCustomerStrategy } from './factories/customer/create-google-pay-tdonlinemart-customer-strategy';
 
 export { default as createGooglePayBraintreeButtonStrategy } from './factories/button/create-google-pay-braintree-button-strategy';
 export { default as createGooglePayPayPalCommerceButtonStrategy } from './google-pay-paypal-commerce/create-google-pay-paypal-commerce-button-strategy';
