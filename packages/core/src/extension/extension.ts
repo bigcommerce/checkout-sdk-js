65ed414eea1d694ca181f6781beaf5fb12412a9c
Peng <peng.zhou@bigcommerce.com>
2024-07-09 12:39:54 +1000

    feat(checkout): CHECKOUT-8281 Add 3 New Checkout Extension Regions

--- packages/core/src/extension/extension.ts
+++ packages/core/src/extension/extension.ts
@@ -13,4 +13,7 @@ export interface ExtensionIframeConfig {
 export const enum ExtensionRegion {
     ShippingShippingAddressFormBefore = 'shipping.shippingAddressForm.before',
     ShippingShippingAddressFormAfter = 'shipping.shippingAddressForm.after',
+    ShippingSelectedShippingMethod = 'shipping.selectedShippingMethod',
+    SummaryAfter = 'summary.after',
+    SummaryLastItemAfter = 'summary.lastItem.after',
 }
