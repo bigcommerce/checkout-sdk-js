046caf51703433cbf09dd5aa288d7b025ca2912c
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2024-08-05 16:59:54 +0300

    refactor(payment): PAYPAL-4531 removed ts-ignore and updated tests in core/src/coupon folder (#2590)

--- packages/core/src/coupon/gift-certificate-request-sender.spec.ts
+++ packages/core/src/coupon/gift-certificate-request-sender.spec.ts
@@ -22,15 +22,8 @@ describe('Gift Certificate Request Sender', () => {
     beforeEach(() => {
         requestSender = createRequestSender();
 
-        // TODO: remove ts-ignore and update test with related type (PAYPAL-4383)
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        jest.spyOn(requestSender, 'delete').mockReturnValue(Promise.resolve());
-
-        // TODO: remove ts-ignore and update test with related type (PAYPAL-4383)
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        jest.spyOn(requestSender, 'post').mockReturnValue(Promise.resolve());
+        jest.spyOn(requestSender, 'delete').mockReturnValue(Promise.resolve(getResponse({})));
+        jest.spyOn(requestSender, 'post').mockReturnValue(Promise.resolve(getResponse({})));
 
         giftCertificateRequestSender = new GiftCertificateRequestSender(requestSender);
     });
