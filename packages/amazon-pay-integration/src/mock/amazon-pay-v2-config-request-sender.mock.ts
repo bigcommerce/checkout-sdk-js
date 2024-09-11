432360f47fdd787265fd251235ebd5c352187055
PavlenkoM <mykhailo.pavlenko@bigcommerce.com>
2024-09-11 20:07:25 +0300

    feat(payment): PI-2549 Move AmazonPay button strategy

--- packages/amazon-pay-integration/src/mock/amazon-pay-v2-config-request-sender.mock.ts
+++ packages/amazon-pay-integration/src/mock/amazon-pay-v2-config-request-sender.mock.ts
@@ -1,5 +1,11 @@
 import { CheckoutConfig } from '../amazon-pay-v2-request-sender';
 
+export function getAmazonPayV2RequestSenderMock() {
+    return {
+        createCheckoutConfig: jest.fn(() => Promise.resolve({ body: getCheckoutRequestConfig() })),
+    };
+}
+
 export function getCheckoutRequestConfig(): CheckoutConfig {
     return {
         payload: 'payload',
