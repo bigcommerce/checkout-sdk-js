1b0895b7821c33a972cef097a3711d56cc330922
PavlenkoM <mykhailo.pavlenko@bigcommerce.com>
2024-09-13 17:34:42 +0300

    feat(payment): PI-2550 AmazonPay strategies from core

--- packages/amazon-pay-integration/src/index.ts
+++ packages/amazon-pay-integration/src/index.ts
@@ -1,3 +1,8 @@
+export { WithAmazonPayV2ButtonInitializeOptions } from './amazon-pay-v2-button-options';
 export { default as createAmazonPayV2ButtonStrategy } from './create-amazon-pay-v2-button-strategy';
+
+export { WithAmazonPayV2CustomerInitializeOptions } from './amazon-pay-v2-customer-initialize-options';
 export { default as createAmazonPayV2CustomerStrategy } from './create-amazon-pay-v2-customer-strategy';
+
+export { WithAmazonPayV2PaymentInitializeOptions } from './amazon-pay-v2-payment-initialize-options';
 export { default as createAmazonPayV2PaymentStrategy } from './create-amazon-pay-v2-payment-strategy';
