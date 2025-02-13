a501a8cad1066a227ecac6346e1811e08bf447db
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2025-02-13 18:49:31 +0200

    refactor(payment): PAYPAL-2609 moved BraintreePayPalButtonStrategy from core to braintree-integration package

--- packages/payment-integration-api/src/is-resolvable-module.ts
+++ packages/payment-integration-api/src/is-resolvable-module.ts
@@ -3,5 +3,5 @@ import ResolvableModule from './resolvable-module';
 export default function isResolvableModule<TModule, TIdentifier>(
     module: TModule,
 ): module is ResolvableModule<TModule, TIdentifier> {
-    return 'resolveIds' in module;
+    return module && 'resolveIds' in module;
 }
