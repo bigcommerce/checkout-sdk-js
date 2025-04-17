e22a1246b428558baf78bd4ce310de05071775f1
SerhiiFilkovskyi <serhii.filkovskiy@bigcommerce.com>
2025-04-17 16:47:11 +0300

    feat(integration): PAYPAL-5359 remove redundant files

--- packages/bcpay-integration/src/lib/bcpay-integration.spec.ts
+++
@@ -1,7 +0,0 @@
-import { bcpayIntegration } from './bcpay-integration';
-
-describe('bcpayIntegration', () => {
-    it('should work', () => {
-        expect(bcpayIntegration()).toBe('bcpay-integration');
-    });
-});
