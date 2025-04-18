5229391a3995fc5f77d47bb59e7fe23b73aa900b
mykola.dronov <mykola.dronov@bigcommerce.com>
2025-04-18 11:42:36 +0200

    feat(payment): Added loadingIndicator to the google-pay-payment-strategy

--- packages/ui/src/loading-indicator/index.ts
+++ packages/ui/src/loading-indicator/index.ts
@@ -1,2 +1,3 @@
 export { default as LoadingIndicator } from './loading-indicator';
+export { DEFAULT_CONTAINER_STYLES } from './loading-indicator';
 export { LoadingIndicatorStyles } from './loading-indicator-styles';
