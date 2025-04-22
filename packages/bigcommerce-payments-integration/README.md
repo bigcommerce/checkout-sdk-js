b0a5dc11d347de5ef3be7f71ea149c8528c8abe7
SerhiiFilkovskyi <serhii.filkovskiy@bigcommerce.com>
2025-04-22 15:42:26 +0300

    feature(payment): create blank package bigcommerce-payments-integration

---
+++ packages/bigcommerce-payments-integration/README.md
@@ -0,0 +1,11 @@
+# bigcommerce-payments-integration
+
+This package contains the integration layer for the BigCommerce provider.
+
+## Running unit tests
+
+Run `npx nx test bigcommerce-payments-integration` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `npx nx lint bigcommerce-payments-integration` to execute the lint via [ESLint](https://eslint.org/).
--- packages/bigcommerce-payments-integration/jest.config.js
+++ packages/bigcommerce-payments-integration/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    displayName: 'bcpay-integration',
+    displayName: 'bigcommerce-payments-integration',
     preset: '../../jest.preset.js',
     globals: {
         'ts-jest': {
@@ -10,5 +10,5 @@ module.exports = {
         '^.+\\.[tj]sx?$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    coverageDirectory: '../../coverage/packages/bcpay-integration',
+    coverageDirectory: '../../coverage/packages/bigcommerce-payments-integration',
 };
