e78da1678e23e2bd8da3b3871cb88fa855aca432
SerhiiFilkovskyi <serhii.filkovskiy@bigcommerce.com>
2025-05-13 15:47:06 +0300

    feature(payment): clone bigcommerce-payments files

---
+++ packages/bigcommerce-payments-integration/src/index.ts
@@ -0,0 +1,3 @@
+export { default as BigCommercePaymentsIntegrationService } from './bigcommerce-payments-integration-service';
+export { default as BigCommercePaymentsRequestSender } from './bigcommerce-payments-request-sender';
+export { default as BigCommercePaymentsScriptLoader } from './bigcommerce-payments-script-loader';
---
+++ packages/bigcommerce-payments-integration/src/mocks/get-billing-address-from-order-details.mock.ts
@@ -0,0 +1,23 @@
+import { BillingAddressRequestBody } from '@bigcommerce/checkout-sdk/payment-integration-api';
+
+import getBigCommercePaymentsOrderDetails from './get-bigcommerce-payments-order-details.mock';
+
+export default function getBillingAddressFromOrderDetails(): BillingAddressRequestBody {
+    const { payer } = getBigCommercePaymentsOrderDetails();
+
+    return {
+        firstName: payer.name.given_name,
+        lastName: payer.name.surname,
+        email: payer.email_address,
+        phone: payer.phone?.phone_number.national_number || '',
+        company: '',
+        address1: payer.address.address_line_1,
+        address2: payer.address.address_line_2,
+        city: payer.address.admin_area_2,
+        countryCode: payer.address.country_code,
+        postalCode: payer.address.postal_code,
+        stateOrProvince: '',
+        stateOrProvinceCode: payer.address.admin_area_1 || '',
+        customFields: [],
+    };
+}
