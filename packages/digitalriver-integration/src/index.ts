b4c6493803ac1c4ffb84b981e21d94f968403303
Vitalii Koshovyi <79574476+vitalii-koshovyi@users.noreply.github.com>
2025-06-16 12:04:17 +0300

    refactor(payment): PI-3957 DigitalRiver deprecation (#2896)

--- packages/digitalriver-integration/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as DigitalRiverPaymentStrategy } from './digitalriver-payment-strategy';
-export { default as DigitalRiverPaymentInitializeOptions } from './digitalriver-payment-initialize-options';
-export { default as DigitalRiverScriptLoader } from './digitalriver-script-loader';
-export { default as createDigitalRiverPaymentStrategy } from './create-digitalriver-payment-strategy';
