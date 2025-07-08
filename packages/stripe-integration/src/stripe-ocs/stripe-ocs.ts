5a018217372d9e98ea7dc735cef3bad26afdb685
mykola.dronov <mykola.dronov@bigcommerce.com>
2025-07-08 14:06:30 +0200

    feat(payment): Stripe Link v2 onConfirm method

--- packages/stripe-integration/src/stripe-ocs/stripe-ocs.ts
+++ packages/stripe-integration/src/stripe-ocs/stripe-ocs.ts
@@ -2,7 +2,31 @@ import { LineItem } from '../stripe-utils';
 
 export interface StripeLinkV2Event {
     value?: null;
+    billingDetails?: {
+        name?: string;
+        email?: string;
+        phone?: string;
+        address?: {
+            line1?: string;
+            city?: string;
+            country?: string;
+            postal_code?: string;
+            state?: string;
+        };
+    };
+    shippingAddress?: {
+        name?: string;
+        address?: {
+            line1?: string;
+            line2?: string;
+            city?: string;
+            country?: string;
+            postal_code?: string;
+            state?: string;
+        };
+    };
     address?: {
+        line1?: string;
         city?: string;
         country?: string;
         postal_code?: string;
