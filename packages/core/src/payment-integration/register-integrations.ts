5d8f86f6bdd3701b645ebee4b59805c743294e94
David Chin <davidchin.yf@gmail.com>
2025-11-11 11:59:14 +1100

    Revert "CHECKOUT-9450: Remove more code around `lazy_strategy` experiment"

--- packages/core/src/payment-integration/register-integrations.ts
+++ packages/core/src/payment-integration/register-integrations.ts
@@ -1,8 +1,12 @@
+import { isEqual } from 'lodash';
+
 import {
     isResolvableModule,
     PaymentIntegrationService,
+    toResolvableModule,
 } from '@bigcommerce/checkout-sdk/payment-integration-api';
 
+import { ErrorLogger } from '../common/error';
 import { ResolveIdRegistry } from '../common/registry';
 
 export type StrategyFactory<TStrategy> = (
@@ -24,7 +28,41 @@ export function registerIntegrations<TStrategy, TResolveId extends { [key: strin
                 return;
             }
 
-            registry.register(resolveId, () => factory(paymentIntegrationService));
+            // TODO: Remove toResolvableModule once CHECKOUT-9450.lazy_load_payment_strategies experiment is rolled out
+            const createStrategy = toResolvableModule(
+                () => factory(paymentIntegrationService),
+                factory.resolveIds,
+            );
+
+            registry.register(resolveId, createStrategy);
         });
     });
+}
+
+// TODO: Remove this function once CHECKOUT-9450.lazy_load_payment_strategies experiment is rolled out
+export function matchExistingIntegrations<TStrategy, TResolveId extends { [key: string]: unknown }>(
+    registry: ResolveIdRegistry<TStrategy, TResolveId>,
+    integrations: Array<StrategyFactory<TStrategy>>,
+    resolveId: TResolveId,
+    errorLogger: ErrorLogger,
+): boolean {
+    const existingFactory = registry.getFactory(resolveId);
+    const matchedExisting = integrations.some(
+        (factory) =>
+            isResolvableModule(existingFactory) &&
+            isResolvableModule(factory) &&
+            isEqual(existingFactory.resolveIds, factory.resolveIds),
+    );
+
+    // During the initial rollout, all strategies will continue to be registered with `strategyRegistryV2`
+    // and bundled together by default. This allows us to compare the passed-in strategies with the existing
+    // ones to ensure they match. Once confirmed, we can remove the comparison logic and the existing strategies,
+    // relying solely on the passed-in strategies.
+    if (existingFactory && !matchedExisting) {
+        const message = `A different strategy is registered for ${JSON.stringify(resolveId)}.`;
+
+        errorLogger.log(new Error(message));
+    }
+
+    return matchedExisting;
 }
