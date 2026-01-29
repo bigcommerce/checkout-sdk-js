d532c6d93e6cc0f9bc9970f7d23982bc25164752
PavlenkoM <mykhailo.pavlenko@bigcommerce.com>
2026-01-29 15:22:43 +0200

    feat(payment): add Stripe Checkout Session payment strategy

--- packages/stripe-integration/src/stripe-cs/stripe-cs-payment-strategy.ts
+++ packages/stripe-integration/src/stripe-cs/stripe-cs-payment-strategy.ts
@@ -1,36 +1,63 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
+    MissingDataError,
+    MissingDataErrorType,
+    NotInitializedError,
+    NotInitializedErrorType,
     OrderFinalizationNotRequiredError,
-    OrderRequestBody,
     PaymentInitializeOptions,
     PaymentIntegrationService,
-    PaymentRequestOptions,
     PaymentStrategy,
 } from '@bigcommerce/checkout-sdk/payment-integration-api';
 import {
+    isStripePaymentMethodLike,
+    StripeClient,
+    StripeElements,
+    StripeInitializationData,
     StripeIntegrationService,
+    StripeJsVersion,
     StripeScriptLoader,
 } from '@bigcommerce/checkout-sdk/stripe-utils';
 
-import { WithStripeCSPaymentInitializeOptions } from './stripe-cs-initialize-options';
+import StripeCSPaymentInitializeOptions, {
+    WithStripeCSPaymentInitializeOptions,
+} from './stripe-cs-initialize-options';
 
 export default class StripeOCSPaymentStrategy implements PaymentStrategy {
+    private stripeClient?: StripeClient;
+    private stripeElements?: StripeElements;
+
     constructor(
-        private _paymentIntegrationService: PaymentIntegrationService,
-        private _scriptLoader: StripeScriptLoader,
-        private _stripeIntegrationService: StripeIntegrationService,
+        private readonly paymentIntegrationService: PaymentIntegrationService,
+        private readonly scriptLoader: StripeScriptLoader,
+        private readonly stripeIntegrationService: StripeIntegrationService,
     ) {}
 
     async initialize(
-        _options: PaymentInitializeOptions & WithStripeCSPaymentInitializeOptions,
+        options: PaymentInitializeOptions & WithStripeCSPaymentInitializeOptions,
     ): Promise<void> {
-        return Promise.resolve();
+        const { stripeocs, methodId, gatewayId } = options;
+
+        if (!stripeocs?.containerId || !gatewayId) {
+            throw new NotInitializedError(NotInitializedErrorType.PaymentNotInitialized);
+        }
+
+        try {
+            await this._initializeStripeElement(stripeocs, gatewayId, methodId);
+        } catch (error) {
+            if (error instanceof Error) {
+                stripeocs.onError?.(error);
+            }
+        }
+
+        this.stripeIntegrationService.initCheckoutEventsSubscription(
+            gatewayId,
+            methodId,
+            stripeocs,
+            this.stripeElements,
+        );
     }
 
-    async execute(
-        _orderRequest: OrderRequestBody,
-        _options?: PaymentRequestOptions,
-    ): Promise<void> {
+    async execute(): Promise<void> {
         return Promise.resolve();
     }
 
@@ -40,5 +67,54 @@ export default class StripeOCSPaymentStrategy implements PaymentStrategy {
 
     deinitialize(): Promise<void> {
         return Promise.resolve();
+    }
+
+    private async _initializeStripeElement(
+        _stripe: StripeCSPaymentInitializeOptions,
+        gatewayId: string,
+        methodId: string,
+    ) {
+        let paymentMethod = this.paymentIntegrationService
+            .getState()
+            .getPaymentMethodOrThrow<StripeInitializationData>(methodId, gatewayId);
+
+        if (!paymentMethod?.clientToken) {
+            const state = await this.paymentIntegrationService.loadPaymentMethod(gatewayId, {
+                params: { method: methodId },
+            });
+
+            paymentMethod = state.getPaymentMethodOrThrow<StripeInitializationData>(
+                methodId,
+                gatewayId,
+            );
+        }
+
+        if (!isStripePaymentMethodLike(paymentMethod)) {
+            throw new MissingDataError(MissingDataErrorType.MissingPaymentMethod);
+        }
+
+        const { clientToken, initializationData } = paymentMethod;
+
+        if (!clientToken) {
+            throw new MissingDataError(MissingDataErrorType.MissingPaymentMethod);
+        }
+
+        this.stripeClient = await this._loadStripeJs(initializationData);
+    }
+
+    private async _loadStripeJs(
+        initializationData: StripeInitializationData,
+    ): Promise<StripeClient> {
+        if (this.stripeClient) {
+            return this.stripeClient;
+        }
+
+        const state = this.paymentIntegrationService.getState();
+
+        return this.scriptLoader.getStripeClient(
+            initializationData,
+            state.getCartLocale(),
+            StripeJsVersion.CLOVER,
+        );
     }
 }
