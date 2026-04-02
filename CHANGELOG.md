f2b88977c6dc7da8999500d13b1647529952f252
bc-travis-ci <bc-travis-ci@bigcommerce.com>
2026-04-02 00:12:55 +0000

    chore(release): 1.894.1 [skip ci]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### [1.894.1](https://github.com/bigcommerce/checkout-sdk-js/compare/v1.894.0...v1.894.1) (2026-04-02)
+
 ## [1.894.0](https://github.com/bigcommerce/checkout-sdk-js/compare/v1.893.0...v1.894.0) (2026-04-01)
 
 
--- dist/types/checkout-sdk.d.ts
+++ dist/types/checkout-sdk.d.ts
@@ -6372,10 +6372,6 @@ declare interface LanguageConfig {
     locale: string;
     locales: Locales;
     translations: Translations;
-    /**
-     * @hidden This property is intended for toggling an experimental change only.
-     */
-    isCspNonceExperimentEnabled?: boolean;
 }
 
 /**
@@ -6393,7 +6389,6 @@ declare class LanguageService {
     private _locales;
     private _translations;
     private _formatters;
-    private _isCspNonceExperimentEnabled;
     /**
      * Remaps a set of language strings with a different set of keys.
      *
