6899dbf415189057b8d4f8ed7554862037c3b981
mykola.dronov <mykola.dronov@bigcommerce.com>
2026-04-09 11:17:02 +0200

    feat(payment): Cybersource unified checkout draft

--- packages/cybersource-integration/src/cybersourcev2/cybersourcev2-payment-strategy.ts
+++ packages/cybersource-integration/src/cybersourcev2/cybersourcev2-payment-strategy.ts
@@ -1,45 +1,75 @@
-import { CardinalThreeDSecureFlowV2 } from '@bigcommerce/checkout-sdk/cardinal-integration';
-import { CreditCardPaymentStrategy } from '@bigcommerce/checkout-sdk/credit-card-integration';
 import {
+    InvalidArgumentError,
+    MissingDataError,
+    MissingDataErrorType,
+    OrderFinalizationNotRequiredError,
     OrderRequestBody,
+    PaymentArgumentInvalidError,
     PaymentInitializeOptions,
     PaymentIntegrationService,
     PaymentRequestOptions,
+    PaymentStrategy,
 } from '@bigcommerce/checkout-sdk/payment-integration-api';
 
-export default class CyberSourceV2PaymentStrategy extends CreditCardPaymentStrategy {
+import CybersourceUnifiedCheckoutClient from './cybersource-unified-checkout-client';
+
+export interface WithCyberSourceV2PaymentInitializeOptions {
+    cybersourcev2?: {
+        containerId: string;
+    };
+}
+
+export default class CyberSourceV2PaymentStrategy implements PaymentStrategy {
     constructor(
-        _paymentIntegrationService: PaymentIntegrationService,
-        private _threeDSecureFlow: CardinalThreeDSecureFlowV2,
-    ) {
-        super(_paymentIntegrationService);
-    }
+        private _paymentIntegrationService: PaymentIntegrationService,
+        private _unifiedCheckoutClient: CybersourceUnifiedCheckoutClient,
+    ) {}
 
-    async initialize(options: PaymentInitializeOptions): Promise<void> {
-        await super.initialize(options);
-
-        const { getPaymentMethodOrThrow } = this._paymentIntegrationService.getState();
-        const paymentMethod = getPaymentMethodOrThrow(options.methodId);
-
-        if (paymentMethod.config.is3dsEnabled) {
-            await this._threeDSecureFlow.prepare(paymentMethod);
-        }
-    }
-
-    async execute(payload: OrderRequestBody, options?: PaymentRequestOptions): Promise<void> {
-        const { payment: { methodId = '' } = {} } = payload;
-        const { getPaymentMethodOrThrow } = this._paymentIntegrationService.getState();
-        const paymentMethod = getPaymentMethodOrThrow(methodId);
-
-        if (paymentMethod.config.is3dsEnabled) {
-            return this._threeDSecureFlow.start(
-                super.execute.bind(this),
-                payload,
-                options,
-                this._hostedForm,
+    async initialize(
+        options: PaymentInitializeOptions & WithCyberSourceV2PaymentInitializeOptions,
+    ): Promise<void> {
+        const { methodId, cybersourcev2 } = options;
+        console.log('init');
+        if (!cybersourcev2?.containerId) {
+            throw new InvalidArgumentError(
+                'cybersourcev2.containerId is required to initialize Cybersource Unified Checkout',
             );
         }
 
-        return super.execute(payload, options);
+        const { getPaymentMethodOrThrow } = this._paymentIntegrationService.getState();
+        const paymentMethod = getPaymentMethodOrThrow(methodId);
+        console.log('paymentMethod', paymentMethod);
+        if (!paymentMethod.clientToken) {
+            throw new MissingDataError(MissingDataErrorType.MissingPaymentMethod);
+        }
+
+        await this._unifiedCheckoutClient.load(paymentMethod.config.testMode);
+        this._unifiedCheckoutClient.initialize(paymentMethod.clientToken, cybersourcev2.containerId);
+    }
+
+    async execute(payload: OrderRequestBody, options?: PaymentRequestOptions): Promise<void> {
+        const { payment, ...order } = payload;
+
+        if (!payment || !payment.methodId) {
+            throw new PaymentArgumentInvalidError(['payment.methodId']);
+        }
+
+        const transientToken = await this._unifiedCheckoutClient.createTransientToken();
+
+        await this._paymentIntegrationService.submitOrder(order, options);
+        await this._paymentIntegrationService.submitPayment({
+            ...payment,
+            paymentData: { nonce: transientToken },
+        });
+    }
+
+    deinitialize(): Promise<void> {
+        this._unifiedCheckoutClient.teardown();
+
+        return Promise.resolve();
+    }
+
+    finalize(): Promise<void> {
+        return Promise.reject(new OrderFinalizationNotRequiredError());
     }
 }
