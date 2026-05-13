55ff24e4309df6db6702e66f6f6c3d3232a68164
Ihor Boiko <payk.ace@gmail.com>
2026-05-13 11:44:14 +0300

    chore(payment): PI-5269 [Wepay] Remove code related to Wepay (#3246)

--- packages/core/src/payment/strategies/wepay/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as WepayPaymentStrategy } from './wepay-payment-strategy';
-export { default as WepayRiskClient } from './wepay-risk-client';
--- packages/core/src/payment/strategies/wepay/wepay-window.ts
+++
@@ -1,7 +0,0 @@
-import WepayRisk from './wepay-risk';
-
-export default interface WepayWindow extends Window {
-    WePay: {
-        risk: WepayRisk;
-    };
-}
