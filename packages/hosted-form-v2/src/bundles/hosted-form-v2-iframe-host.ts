9c959410c8921481c96ad6046a0dac9f79bad91c
Tharaa Elmorssi <tharaa.elmorssi@bigcommerce.com>
2026-05-14 14:20:45 +1000

    feat(payment): PAYMENTS-11630 Expose components consumed by new manual order in newer releases

--- packages/hosted-form-v2/src/bundles/hosted-form-v2-iframe-host.ts
+++ packages/hosted-form-v2/src/bundles/hosted-form-v2-iframe-host.ts
@@ -1,2 +1,9 @@
 export { createHostedFormService } from '../create-hosted-form-service';
 export { default as createStoredCardHostedFormService } from '../create-hosted-form-stored-card-service';
+export { default as HostedFormService } from '../hosted-form-service';
+export { HostedCardFieldOptionsMap } from '../hosted-form-options';
+export {
+    HostedInputSubmitManualOrderSuccessEvent,
+    HostedInputValidateErrorDataMap,
+    HostedInputValidateResults,
+} from '../iframe-content';
