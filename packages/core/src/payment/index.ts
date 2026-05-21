2fd277f162fb43d34a127e0572f3a6312119780d
Peng Zhou <peng.zhou@bigcommerce.com>
2026-05-21 13:25:51 +1000

    Remove unused exports

--- packages/core/src/payment/index.ts
+++ packages/core/src/payment/index.ts
@@ -37,11 +37,7 @@ export {
     WithDocumentInstrument,
     WithMollieIssuerInstrument,
 } from './payment';
-export {
-    default as B2BCompanyPaymentMethodRequestSender,
-    B2BCompanyPaymentMethodsResponseBody,
-} from './b2b-company-payment-method-request-sender';
-export { default as filterPaymentMethodsByB2BCompanyAllowList } from './b2b-company-payment-method-filter-transformer';
+export { default as B2BCompanyPaymentMethodRequestSender } from './b2b-company-payment-method-request-sender';
 export { default as PaymentMethod } from './payment-method';
 export { default as PaymentMethodMeta } from './payment-method-meta';
 export { default as PaymentMethodConfig } from './payment-method-config';
