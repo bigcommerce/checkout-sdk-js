de8823822503b486c386400d758e3246222e83a7
Rabin Shrestha <rabin.shrestha@bigcommerce.com>
2026-06-02 21:19:20 +1000

    feat(payment): PAYMENTS-11661 Add payment method config to indicate support for vaulting all payments

--- packages/apple-pay-integration/src/mocks/apple-pay-method.mock.ts
+++ packages/apple-pay-integration/src/mocks/apple-pay-method.mock.ts
@@ -14,6 +14,7 @@ export function getApplePay(): PaymentMethod {
             isVaultingEnabled: false,
             hasDefaultStoredInstrument: false,
             isHostedFormEnabled: false,
+            shouldVaultAllPayments: false,
         },
         type: 'PAYMENT_TYPE_WALLET',
         initializationStrategy: {
